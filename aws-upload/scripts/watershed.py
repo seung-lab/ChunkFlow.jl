6e510b1358a32295ea5771021736f320ca07ab2c
Ignacio Tartavull <tartavull@princeton.edu>
2015-02-27 17:01:14 -0500

    move memory and thread parameters to the top, and some fixies for watershed
    Former-commit-id: e81ec57bba602db252c26904b91c33a1df38a68d

--- aws-upload/scripts/watershed.py
+++ aws-upload/scripts/watershed.py
@@ -1,3 +1,7 @@
+memory =  10 * 10**9#gb
+threads = 8
+
+
 import os
 import numpy
 import re
@@ -23,7 +27,7 @@ for chunk_dir in os.listdir('../data/'):
 		print Exception("There is no stage 2 output in chunk {0}".format(chunk_dir))
 
   	m = re.match(r'z(\d+)-y(\d+)-x(\d+)',chunk_dir)
-	z = m.group(1) ; y = m.group(2); x = m.group(3)
+	z = int(m.group(1)) ; y = int(m.group(2)); x = int(m.group(3))
 	
 	if z > max_z:
 		max_z = z
@@ -35,15 +39,16 @@ for chunk_dir in os.listdir('../data/'):
 		max_x = x
 
 
+znn_chunks = (max_x+1) * (max_y+1) * (max_z+1)
 #The larger the chuncks watershed process in parallel the better
 #Divide the avaiable memory by the number of threads, that should be the size of a chunk.
 #Watershed requires about 20 bytes per voxel.
-memory =  200 * 10**9#gb
-thread = 16 
-chunk_max_memory = memory / 16 / 20 
+
+chunk_max_memory = memory / threads / 20 
 
 #Compute this based on chunkMaxSize and  chunk_max_memory
 chunk_divs = numpy.ceil(max_chunk_size/chunk_max_memory).astype(int)
+print 'We will divide each znn chunk in {0} parts, because we have {1} znn chunks, we will have {2} watershed chunks in total'.format(chunk_divs,znn_chunks,chunk_divs * znn_chunks)
 
 if not os.path.exists('../watershed/data'):
 	os.makedirs('../watershed/data')
@@ -59,32 +64,36 @@ os.makedirs('../watershed/data/input.chunks')
 
 #Iterate trough znn chunks
 zabs = 0
-for z_znn in range(max_x):
+for z_znn in range(max_z+1):
 	yabs = 0
-	for y_znn in range(max_y):
+	for y_znn in range(max_y+1):
 		xabs = 0
-		for x_znn in range(max_z):
+		for x_znn in range(max_x+1):
 
 			os.makedirs('../watershed/data/input.chunks/{0}/{1}/{2}'.format(x_znn,y_znn,z_znn))
 
 			#Load znn chunks and concatenate it in one affinity, we assume this can fit in memory
 			#If it doesn't we should make znn chunks smaller
-			znn_chunk_size = numpy.prod(numpy.fromfile( '../data/{0}/output/stage21.0.size'.format(chunk_dir), dtype='uint32'))
-			znn_chunk_0 =  numpy.fromfile( '../data/{0}/output/stage21.0'.format(chunk_dir), dtype='uint32').reshape(znn_chunk_size)
-			znn_chunk_1 =  numpy.fromfile( '../data/{0}/output/stage21.1'.format(chunk_dir), dtype='uint32').reshape(znn_chunk_size)
-			znn_chunk_2 =  numpy.fromfile( '../data/{0}/output/stage21.2'.format(chunk_dir), dtype='uint32').reshape(znn_chunk_size)
+			znn_chunk_size = numpy.fromfile('../data/{0}/output/stage21.0.size'.format(chunk_dir), dtype='uint32')
+			znn_chunk_0 =  numpy.fromfile('../data/{0}/output/stage21.0'.format(chunk_dir), dtype='double').reshape(znn_chunk_size)
+			znn_chunk_1 =  numpy.fromfile('../data/{0}/output/stage21.1'.format(chunk_dir), dtype='double').reshape(znn_chunk_size)
+			znn_chunk_2 =  numpy.fromfile('../data/{0}/output/stage21.2'.format(chunk_dir), dtype='double').reshape(znn_chunk_size)
 			znn_chunk_affinity = numpy.concatenate((znn_chunk_0[None,...],znn_chunk_1[None,...],znn_chunk_2[None,...]), axis=0)
 
 			z_max = znn_chunk_size[0]
+		
 			for z_chunk_max in numpy.linspace(0, z_max , chunk_divs +1):
+
+				z_chunk_max = z_chunk_max.astype(int)
 
 				if z_chunk_max == 0:
 					z_chunk_min = 0
-				continue
+					continue
 				
 				print 'prepared chunk {0}:{1}:{2} size: [ {3} {4} {5} ]'.format(xabs, yabs, zabs, znn_chunk_size[2], znn_chunk_size[1], znn_chunk_size[0])
 				
 				affin = znn_chunk_affinity[:,z_chunk_min:z_chunk_max,:,:]
+				print affin.shape
 				affinities.write(affin.tostring())
 				sz = numpy.asarray(affin.shape).astype('int32')
 				sizes.write(sz.tostring())
@@ -92,7 +101,7 @@ for z_znn in range(max_x):
 				#For next loop
 				zabs += 1;
 				z_chunk_min = z_chunk_max
-				
+
 		xabs+=1
 	yabs+=1
 
