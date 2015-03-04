fa6f7b28c2573c7b407ef3326415306dddb22e33
jingpengwu <jingpeng.wu@gmail.com>
2015-03-04 15:44:52 -0500

    merge chunks and produce h5 files
    Former-commit-id: b975ae7fd0f645991896dd1cce40b10638f3ccd3
    Former-commit-id: 480f4812ed755268e70519ffca195cb04ac92815

--- aws-upload/scripts/xxl_watershed_read_global.py
+++ aws-upload/scripts/xxl_watershed_read_global.py
@@ -6,37 +6,6 @@ Created on Fri Feb 27 13:39:22 2015
 """
 import numpy as np
 
-# the first version needs to load all the data into memory
-def xxl_watershed_read_global_V1( filename, s, width, h5filename ):
-    seg = np.zeros( s, dtype='uint32' )
-    xind = 0
-    for x in range(0, s[2], width):
-        yind = 0
-        for y in range(0, s[1], width):
-            zind = 0
-            for z in range(0, s[0], width):
-                cto = np.minimum( np.array([z,y,x])+width, s-1 );
-                cfrom = np.maximum( np.array([0,0,0]), np.array([z,y,x])-1 )
-                fname = filename + '.chunks/' + str(xind) + '/' + str(yind) + '/' + str(zind) + '/.seg'
-                sze = cto - cfrom + 1
-                chk = np.reshape( np.fromfile(fname, count = np.prod(sze), dtype='uint32' ), sze)
-                seg[ cfrom[0]+1:cto[0], cfrom[1]+1:cto[1], cfrom[2]+1:cto[2] ] = chk[ 1:-1, 1:-1, 1:-1 ]
-                print( "prepared chunk {}:{}:{}, size: {} \n".format(x,y,z, sze) )
-                zind += 1
-            yind += 1
-        xind += 1
-    dend_values = np.fromfile( filename + '.dend_values', dtype='single' )
-    dend = np.fromfile( filename + '.dend_pairs', dtype = 'uint32' )
-    dend = dend.reshape((2, len(dend_values)))
-    # write the h5 file
-    import h5py
-    f = h5py.File( h5filename )
-    f.create_dataset('/main',data=seg)
-    f.create_dataset('/dend', data=dend, dtype='uint32')
-    f.create_dataset('/dendValues', data=dend_values, dtype='single')
-    f.close()
-
-
 def truncate_dend(vol, dend, dendValues):
     segm = np.unique( vol )
     
