41e3329838609de18470f45088da62efd3fa3b0f
Ignacio Tartavull <tartavull@princeton.edu>
2015-03-11 19:29:39 -0400

    beter organization of folders, and some bug fixing
    Former-commit-id: 8b8b0a8dfdb7c96e696640465a6049aec85b311f
    Former-commit-id: fd0c3e71c9ccdc01c49e9346aacee9341f402cb4

---
+++ pipeline/znn/data/z1-y1-x1/trainning_spec/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+		../znn/znn-release/bin/znn --options=../znn/data/z1-y1-x1/trainning_spec/stage1.spec --test_only=1
+		../znn/znn-release/bin/znn --options=../znn/data/z1-y1-x1/trainning_spec/stage2.spec --test_only=1
+		
