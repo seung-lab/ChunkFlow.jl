d17691d6952a30c960e690ecb599985cf1374142
Ignacio Tartavull <tartavull@princeton.edu>
2015-03-12 19:29:57 -0400

    add code for chopping watershed output in many omni projects
    Former-commit-id: 48569fb8d6f5f92dfff4b7f62fcb6dff7e0acf7e
    Former-commit-id: 6dab34ccc9a65d5e1e4029585d45da9ef5904f5a

--- pipeline/omnify/runall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sh chunk_1.sh
--- pipeline/znn/data/z0-y1-x1/trainning_spec/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-		../znn/znn-release/bin/znn --options=../znn/data/z0-y1-x1/trainning_spec/stage1.spec --test_only=1
-		../znn/znn-release/bin/znn --options=../znn/data/z0-y1-x1/trainning_spec/stage2.spec --test_only=1
-		
