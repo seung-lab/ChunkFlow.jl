b04949ee6244a89c00503d59764b887de846075b
jingpengwu <jingpeng.wu@gmail.com>
2015-05-06 15:02:13 -0400

    add watershed scripts and readme content
    Former-commit-id: c141793aa26b8035135c98317e60be290c22c1f8

--- pipeline/scripts/watershed_chop.py
+++ pipeline/scripts/watershed_chop.py
@@ -1,120 +1,66 @@
+import numpy as np
 import os
-import numpy
-import re
-from subprocess import call
 import h5py
-from tqdm import tqdm
-
-from global_vars import *
 import time
-#Read the merged znn output
-f = h5py.File('../znn/data/znn_merged.hdf5', 'r')
-znn = f['/main']
+from global_vars import *
 
-#The larger the chuncks watershed process in parallel the better
-#Divide the avaiable memory by the number of threads, that should be the size of a chunk.
-#Watershed requires about 20 bytes per voxel.
-chunk_max_memory = memory / threads / 20 
+def watershed_chop():
+    #%% prepare the folders
+    if os.path.exists(gtemp_file):
+        os.system('rm -rf ' + gtemp_file + 'input*')
+    os.makedirs(gtemp_file + 'input.chunks/')
 
-#Check how many divs we need, divide each axis the same ammount of times
-required_divs = numpy.prod(znn.shape) /chunk_max_memory + 1.0
-required_divs = int(required_divs) 
+    #%% read hdf5
+    f = h5py.File(gaffin_file, 'r')
+    affin = f['/main']
 
-best_score = numpy.float('inf')
-best_divs = None
-#Minimize overlapping
-for z in range(1,required_divs+1):
-	for y in range(1,required_divs+1):
-		for x in range(1,required_divs+1):
-			
-			if x*y*z > required_divs:
-				continue
+    s = np.array(affin.shape, dtype='uint32')[1:]
+    s.tofile(gtemp_file + 'input.total_size')
 
-			test_divs = numpy.array([z,y,x])
-			score = numpy.std(numpy.asarray(znn.shape)[1:4] / test_divs)
-			if score < best_score:
-				best_score = score
-				best_divs = test_divs
+    # width can not be bigger than total volume size
+    global width
+    width = np.minimum(gwidth, s)
+    width.tofile(gtemp_file + 'input.width')
 
-best_divs = numpy.array([1, 4, 4])
-best_divs = best_divs[::-1]
+    fa = open(gtemp_file + 'input.affinity.data', mode='w+')
 
+    chunkSizes = []
+    chunkid = 0
+    for cidx, x in enumerate(range(0, s[2], gwidth[2]) ):
+        for cidy, y in enumerate(range(0, s[1], gwidth[1])):
+            for cidz, z in enumerate( range(0, s[0], gwidth[0]) ):
+               chunkid += 1
+               print "chunk ID: {}".format(chunkid)
+               start = time.time()
+               cfrom = np.maximum( np.array([z,y,x])-1, np.array([0,0,0]))
+               cto = np.minimum(np.array([z,y,x]) + width + 1, s)
+               size = cto - cfrom
+               chunkSizes.append( size[::-1] )
 
-width = numpy.ceil( znn.shape[1:4][::-1] / best_divs -1 ).astype(int)
+               # the chunk part
+               part = affin[:,cfrom[0]:cto[0], cfrom[1]:cto[1], cfrom[2]:cto[2]]
+               part.tofile(fa)
 
-if not os.path.exists('../watershed/data'):
-	os.makedirs('../watershed/data')
-else:
-	#For test
-	import shutil
-	shutil.rmtree('../watershed/data')
-	os.makedirs('../watershed/data')
+               # create some folders for watershed
+               os.makedirs(gtemp_file + 'input.chunks/{0}/{1}/{2}'.format(cidx,cidy,cidz))
+               print "elapsed time {0:.1f}s".format( time.time()-start )
+    # close the files
+    f.close()
+    fa.close()
 
-    #For production
-    #raise Exception('folder already exists')
+    # metadata and size
+    metadata = np.array([32, 32, cidx+1, cidy+1, cidz+1]).astype('uint32')
+    metadata.tofile(gtemp_file + 'input.metadata')
 
+    chunkSizes = np.array( chunkSizes, dtype='uint32' )
+    chunkSizes.tofile(gtemp_file + 'input.chunksizes')
 
-affinities = open('../watershed/data/input.affinity.data','w+')
-os.makedirs('../watershed/data/input.chunks')
-sizes = []
+    # run watershed
+    #print 'run watershed '
+    os.system(gws_bin_file + " --filename=" + gtemp_file + "input "\
+                + "--high={} --low={} --dust={} --dust_low={} --threads={}".format(gws_high, gws_low, gws_dust, gws_dust_low, gws_threads_num))
 
-total = numpy.prod(best_divs).astype(int) -1
-progressbar = tqdm(range(total))
-
-for x_chunk in range(0, best_divs[0]):
-	if x_chunk == 0:
-		x_chunk_min = 0
-	x_chunk_max = x_chunk_min + width[0]
-
-	for y_chunk in range(0 , best_divs[1]):
-		if y_chunk == 0:
-			y_chunk_min = 0		
-		y_chunk_max = y_chunk_min + width[1]
-
-
-		for z_chunk in range(0, best_divs[2]):
-			if z_chunk == 0:
-				z_chunk_min = 0
-			z_chunk_max = z_chunk_min + width[2]
-
-			os.makedirs('../watershed/data/input.chunks/{0}/{1}/{2}'.format(x_chunk, y_chunk, z_chunk))
-			cfrom = numpy.array([x_chunk_min, y_chunk_min, z_chunk_min]) - 2
-			cfrom[cfrom < 0] = 0
-			cto = numpy.array([x_chunk_max, y_chunk_max, z_chunk_max]) + 1
-			cto = numpy.minimum(cto, znn.shape[1:4][::-1])
-			
-			affin = znn[:,cfrom[2]:cto[2], cfrom[1]:cto[1], cfrom[0]:cto[0]].astype('float32')
-			affin.tofile(affinities)
-
-			sz = numpy.asarray(affin.shape[1:4])[::-1].astype('uint32')
-			sizes.append(sz)
-			
-			print 'prepared chunk (xyz) {0}:{1}:{2} , position [{3}-{4} , {5}-{6}, {7}-{8}] size: [ {9} {10} {11} ]'.format(x_chunk, y_chunk, z_chunk, cfrom[0],cto[0],cfrom[1], cto[1],cfrom[2], cto[2], sz[2], sz[1], sz[0])
-			
-			try:
-				progressbar.next()
-			except:
-				pass
-
-			z_chunk_min = z_chunk_max
-		y_chunk_min = y_chunk_max
-	x_chunk_min = x_chunk_max
-
-affinities.close()
-
-chunksizes = open('../watershed/data/input.chunksizes','w+')
-numpy.asarray(sizes).flatten().tofile(chunksizes)
-chunksizes.close()
-
-#Write metedata to file
-#however the max ID is limited to 31 bits ~ 2 billion]  if using uint32s then the volumes should be <= 2G voxels just in case
-#i guess i wouldnt make cubes bigger than 1k x 1k x 2k
-# unless we want to use uint64s for IDs
-metadata = numpy.concatenate(( numpy.array([32, 32]), best_divs)).astype('int32')
-metadata.tofile('../watershed/data/input.metadata')
-
-#Run watershed
-print 'running watershed ...'
-#there should be no spaces in the arguments, otherwise it doesn't work
-call(["../watershed/src/zi/watershed/main/bin/xxlws", "--filename=../watershed/data/input", "--high=0.90", "--low=0.25", "--dust=400", "--dust_low=0.3"])
-
+#%%
+if __name__ == "__main__":
+    watershed_chop()
+    
