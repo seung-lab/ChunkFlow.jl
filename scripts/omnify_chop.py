043563d0f87f1314ee9fa134b8fd368105b90a8e
jingpengwu <jingpeng.wu@gmail.com>
2015-06-25 15:46:12 -0400

    fix several bugs!
    Former-commit-id: a92d104a8124d47c4a4ae4755e8e5841be231534
    Former-commit-id: 6fe337993e51a58166fa36f2c85ae8262d3f309d

--- scripts/omnify_chop.py
+++ scripts/omnify_chop.py
@@ -27,18 +27,18 @@ def write_cmd( fname, bfrom ):
     cmdfname = gomnify_data_file + fname + ".cmd"
     fcmd = open(cmdfname, 'w')
     fcmd.write('create:'+ gomniprojects_save_file + fname+'.omni\n')
-    fcmd.write('loadHDF5chann:' + fname + '.chann.h5\n')
+    fcmd.write('loadHDF5chann:' + gchann_file + '\n')
     fcmd.write('setChanResolution:1,{},{},{}\n'.format( gvoxel_size[2], gvoxel_size[1], gvoxel_size[0] ))
-    fcmd.write('setChanAbsOffset:,1,{},{},{}\n'.format(\
-        bfrom[2]*gvoxel_size[2],\
-        bfrom[1]*gvoxel_size[1],\
-        bfrom[0]*gvoxel_size[0]) )
+    # fcmd.write('setChanAbsOffset:,1,{},{},{}\n'.format(\
+    #     bfrom[2]*gvoxel_size[2],\
+    #     bfrom[1]*gvoxel_size[1],\
+    #     bfrom[0]*gvoxel_size[0]) )
     fcmd.write('loadHDF5seg:'+ fname +'.segm.h5\n')
     fcmd.write('setSegResolution:1,{},{},{}\n'.format( gvoxel_size[2], gvoxel_size[1], gvoxel_size[0] ))
-    fcmd.write('setSegAbsOffset:1,{},{},{}\n'.format(\
-        bfrom[2]*gvoxel_size[2],\
-        bfrom[1]*gvoxel_size[1],\
-        bfrom[0]*gvoxel_size[0]) )
+    # fcmd.write('setSegAbsOffset:1,{},{},{}\n'.format(\
+    #     bfrom[2]*gvoxel_size[2],\
+    #     bfrom[1]*gvoxel_size[1],\
+    #     bfrom[0]*gvoxel_size[0]) )
     fcmd.write('mesh\n')
     fcmd.write('quit\n\n')
     fcmd.close()
@@ -57,15 +57,18 @@ def write_runall_sh(blockNum):
     fsh.close()
 
 def prepare_complete_volume():
+    faffin = h5py.File( gaffin_file, 'r' )
+    affin = faffin['/main']
+    s = np.array(affin.shape, dtype='uint32')[1:]
+    faffin.close()
     fname = "chunk_" + str(1) + \
-            "_Z" + str(0) + '-' + str(s[0]-1) + \
+            "_X" + str(0) + '-' + str(s[2]-1) + \
             "_Y" + str(0) + '-' + str(s[1]-1) + \
-            "_X" + str(0) + '-' + str(s[2]-1)
+            "_Z" + str(0) + '-' + str(s[0]-1)
     # write the segmentation h5 file
     h5fname = gomnify_data_file + fname + ".segm.h5"
+    import os
     os.system("mv " + gws_merge_h5 + " " + h5fname)
-    write_h5_with_dend( h5fname, dend, dendValues )
-    # channel data
     # write omnify cmd file
     write_cmd( fname, np.array([0,0,0]) )
     # generate a corresponding sh file
@@ -77,6 +80,7 @@ def prepare_complete_volume():
 def prepare_blocks():
     ftmp = h5py.File( gws_merge_h5, "r" )
     seg = ftmp['/main']
+    s = np.array(seg.shape, dtype='uint32')
     print "get the blocks ..."
     # get the blocks of seg
     fchann = h5py.File( gchann_file, 'r')
@@ -84,7 +88,6 @@ def prepare_blocks():
 
     faffin = h5py.File( gaffin_file, 'r' )
     affin = faffin['/main']
-    s = np.array(affin.shape, dtype='uint32')[1:]
 
     blockid = 0
     for bz in xrange(0, s[0], gblocksize[0]-goverlap[0]):
@@ -101,18 +104,18 @@ def prepare_blocks():
 
                 print "bfrom: {}".format(bfrom)
                 print "bto  : {}".format(bto)
-                start = time.time()
+                start_tmp = time.clock()
                 block = np.asarray( seg[bfrom[0]:bto[0], bfrom[1]:bto[1], bfrom[2]:bto[2] ] )
-                print "reading segment block takes {0:.1f}s".format( time.time() - start )
-                start = time.time()
+                print "reading segment block takes {0:.1f}s".format( time.clock() - start_tmp )
+                start_tmp = time.clock()
                 block_affin = np.asarray( affin[:, bfrom[0]:bto[0], bfrom[1]:bto[1], bfrom[2]:bto[2]] )
-                print "reading affinity block takes {0:.1f}s".format( time.time() - start )
+                print "reading affinity block takes {0:.1f}s".format( time.clock() - start_tmp )
 
                 # rebuild MST
-                start = time.time()
+                start_tmp = time.clock()
                 from relabel import relabel
                 block, chunk_dend, chunk_dendValues = relabel(block, block_affin)
-                print "relabeling takes {0:.1f}s".format( time.time() - start )
+                print "relabeling takes {0:.1f}s".format( time.clock() - start_tmp )
                 print "relabelled block:    max: {}, number: {}".format(block.max(), np.unique(block).shape[0])
                 # write the h5 file
                 fname = "chunk_" + str(blockid) + \
@@ -120,18 +123,18 @@ def prepare_blocks():
                         "_Y" + str(bfrom[1]) + '-' + str(bto[1]-1) + \
                         "_Z" + str(bfrom[0]) + '-' + str(bto[0]-1)
                 # write the segmentation h5 file
-                start = time.time()
+                start_tmp = time.clock()
                 h5fname = gomnify_data_file + fname + ".segm.h5"
                 write_h5_with_dend( h5fname, chunk_dend, chunk_dendValues, block )
-                print "writting segment block takes {0:.1f}s".format( (time.time() - start) )
+                print "writting segment block takes {0:.1f}s".format( (time.clock() - start_tmp) )
 
                 # write channel data
-                start = time.time()
+                start_tmp = time.clock()
                 block_chann = np.asarray( chann[bfrom[0]:bto[0], bfrom[1]:bto[1], bfrom[2]:bto[2] ] )
-                print "reading channel block takes {0:.1f}s".format( time.time() - start )
-                start = time.time()
+                print "reading channel block takes {0:.1f}s".format( time.clock() - start_tmp )
+                start_tmp = time.clock()
                 write_h5_chann( gomnify_data_file + fname + '.chann.h5', block_chann )
-                print "writting channel block takes {0:.1f}s".format( (time.time() - start) )
+                print "writting channel block takes {0:.1f}s".format( (time.clock() - start_tmp) )
 
                 # write omnify cmd file
                 write_cmd( fname, bfrom )
