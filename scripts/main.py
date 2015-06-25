bbe80561a3716cbc56f172098758062b78d15a22
jingpengwu <jingpeng.wu@gmail.com>
2015-06-25 15:46:12 -0400

    fix several bugs!
    Former-commit-id: 8ea75d95e4e91d5e87e543ce1a20352a4efe5d36

--- scripts/main.py
+++ scripts/main.py
@@ -15,41 +15,35 @@ from global_vars import *
 #%% watershed chop
 from watershed_chop import watershed_chop
 print "chopping volume for watershed ..."
-start = time.clock()
-# watershed_chop()
-print "watershed chop time: {0:.1f}m".format( (time.clock()-start)/60 )
+mstart = time.clock()
+watershed_chop()
+print "watershed chop time: {0:.2f}m".format( (time.clock()-mstart)/60 )
 
 #%% run watershed
 print 'run watershed... '
-start = time.clock()
+mstart = time.clock()
 os.system(gws_bin_file + " --filename=" + gtemp_file + "input "\
             + "--high={} --low={} --dust={} --dust_low={} --threads={}".format(gws_high, gws_low, gws_dust, gws_dust_low, gws_threads_num))
-print "watershed time: {0:.1f}m".format( (time.clock()-start)/60 )
+print "watershed time: {0:.2f}m".format( (time.clock()-mstart)/60 )
 
 #%% watershed merge
 from watershed_merge import watershed_merge
 print "merging chunks ..."
-start = time.clock()
+mstart = time.clock()
 watershed_merge()
-print "watershed merge time: {0:.1f}m".format( (time.clock()-start)/60 )
-
-#%% directly omnify the whole image stack
-start = time.clock()
-os.system("cd /data/jingpeng/")
-os.system("sh /data/jingpeng/omnify.sh")
-print "omnification time: {0:.1f}m".format( (time.clock()-start)/60 )
+print "watershed merge time: {0:.2f}m".format( (time.clock()-mstart)/60 )
 
 #%% omnify chop
 from omnify_chop import omnify_chop
 print "chopping volume to build small omni projects..."
-start = time.clock()
-# omnify_chop()
-print "omnify chop time: {0:.1f}m".format( (time.clock() - start)/60 )
+mstart = time.clock()
+omnify_chop()
+print "omnify chop time: {0:.2f}m".format( (time.clock() - mstart)/60 )
 
 #%% start meshing
 from global_vars import gomnify_data_file
 print "omnification ..."
-start = time.clock()
-os.system("cd "+gomnify_data_file)
-# os.system("sh " + gomnify_data_file + "run_all.sh")
-print "omnification takes {0:.1f}m".format( (time.clock() - start)/60 )
+mstart = time.clock()
+# os.system("cd "+gomnify_data_file)
+os.system("sh " + gomnify_data_file + "runall.sh")
+print "omnification takes {0:.2f}m".format( (time.clock() - mstart)/60 )
