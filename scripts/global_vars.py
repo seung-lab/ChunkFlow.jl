9482f74bb10c1c98557085680284158fc3e94d10
jingpengwu <jingpeng.wu@gmail.com>
2015-06-27 14:54:29 -0400

    integrate the convert_to_hdf5 script; fix the omnify filename bug; uncomment the coordinate shift code.
    Former-commit-id: f9675304fe8d6afe0b842da492b92c7ef09303b1

--- scripts/global_vars.py
+++ scripts/global_vars.py
@@ -2,9 +2,9 @@ import numpy as np
 
 #%% basic
 # note that we'd better put the channel and affinity data in local disk since it is IO bound.
-gchann_file = ''
-gaffin_file = '/usr/people/jingpeng/seungmount/research/Jingpeng/09_pypipeline/znn_merged.h5'
-gtemp_file = '/data/jingpeng/temp/'
+gchann_file = 'channel_batch92_2.h5'
+gaffin_file = 'batch92_2.h5'
+gtemp_file = 'temp/'
 
 # the path of omnify binary
 gomnifybin = 'bash ../omnify/omnify.sh'
@@ -25,20 +25,30 @@ fov_stage1 = np.array([1,109,109])
 fov_stage2 = np.array([9,65,65])
 fov_effective = fov_stage1 + fov_stage2 - 1
 
+#%% convert znn output to hdf5 file
+import convert_to_hdf5
+fname = "/usr/people/jingpeng/seungmount/research/Jingpeng/01_ZNN/znn-release/experiments/VeryDeep2HR_w65x9/output2/out92."
+gnet_out_fnames = (fname+"0", fname+"1", fname+"2")
+gchann_fname = "/usr/people/jingpeng/seungmount/research/Jingpeng/01_ZNN/znn-release/dataset/fish/data/batch92.image"
+
 #%% watershed chop
 # step: z,y,x
 gwidth = np.array([2000, 2000, 2000], dtype='uint32')
 # watershed parameters
 gws_bin_file = '../watershed/src/quta/zi/watershed/main/bin/xxlws'
-gws_high = 0.900
+gws_high = 0.880
 gws_low = 0.3
-gws_dust = 100
+gws_dust = 400
 gws_dust_low = 0.25
 # if there are watershed error, we can try to change the threads number to 1
 gws_threads_num = 1
 
+
+# prepare the omnify data for omnifying
+gomnify_data_file = '../omnify/'
+
 #%% watershed merge
-gws_merge_h5 = gtemp_file + "pywsmerge.Th-{}.Tl-{}.Ts-{}.Te-{}.h5".format(int(gws_high*1000), int(gws_low*1000), int(gws_dust), int(gws_dust_low*1000))
+gws_merge_h5 = gomnify_data_file + "pywsmerge.Th-{}.Tl-{}.Ts-{}.Te-{}.h5".format(int(gws_high*1000), int(gws_low*1000), int(gws_dust), int(gws_dust_low*1000))
 
 #%% omnify chop
 # the block size and overlap size, z,y,x
@@ -47,8 +57,5 @@ goverlap = np.array([20,32,32], dtype='uint32')
 # voxel size: z,y,x
 gvoxel_size = np.array([40,7,7])
 
-# prepare the omnify data for omnifying
-gomnify_data_file = '/data/jingpeng/omnify/'
-
 # the save path of omni projects, should be local. Remote path may make the segmentation empty.
-gomniprojects_save_file = '/data/jingpeng/omni_projects/'
+gomniprojects_save_file = '~/omni_projects/'
