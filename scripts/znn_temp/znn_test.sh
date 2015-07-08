c245e0616ccbd99f63c7903a334b85c83010a893
jingpengwu <jingpeng.wu@gmail.com>
2015-07-08 14:38:49 -0400

    remove temp files
    Former-commit-id: 7439d97912315cf477c91760030cfcb736ab229d
    Former-commit-id: 65ac623d26ef5da6b3564e16829cb90c16775b23

--- scripts/znn_temp/znn_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=LD_LIBRARY_PATH:"/opt/boost/lib"
-/usr/people/jingpeng/seungmount/research/Jingpeng/01_ZNN/znn-release/bin/znn --test_only=true --options="general.config"
-    
