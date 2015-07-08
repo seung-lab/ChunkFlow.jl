ed57c1659e8bb7835164a21a1402ce5763156a8c
jingpengwu <jingpeng.wu@gmail.com>
2015-07-08 17:16:06 -0400

    pdate
    Former-commit-id: 0915adcc2d7785d490be550af5022e358fa8989f

---
+++ omnify/chunk_1_X0-845_Y0-845_Z0-118.cmd
@@ -0,0 +1,10 @@
+create:/usr/people/jingpeng/omni_projects/chunk_1_X0-845_Y0-845_Z0-118.omni
+loadHDF5chann:../omnify/channel_batch92.h5
+setChanResolution:1,5,5,45
+setChanAbsOffset:,1,0,0,0
+loadHDF5seg:chunk_1_X0-845_Y0-845_Z0-118.segm.h5
+setSegResolution:1,5,5,45
+setSegAbsOffset:1,0,0,0
+mesh
+quit
+
