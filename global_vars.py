1732810b7970dcd922606b2aa08edea0aae27f59
jingpengwu <jingpeng.wu@gmail.com>
2015-07-13 17:05:12 -0400

    use abspath to coorporate different packages
    Former-commit-id: 1d7ec8547c282f0db8c83d5a148cc44de64d3fb5
    Former-commit-id: f19e0244d7738477b1e8e2935568f5fdf0066e20

--- global_vars.py
+++ global_vars.py
@@ -1,34 +1,38 @@
 import numpy as np
+import os
 
 #%% basic
 # note that we'd better put the channel and affinity data in local disk since it is IO bound.
 # if we put them in remote mounted folder, the chopping could be slow due to IO latency
-gchann_file = 'data/chann_batch92.h5'
-gaffin_file = 'data/affin_batch92.h5'
-gtemp_file = 'tmp/'
+gchann_file = 'data/chann_batch91.h5'
+gaffin_file = 'data/affin_batch91.h5'
+# global temporary folder for whole pipeline
+gabspath = os.path.dirname(os.path.abspath(__file__))
+gtmp = gabspath + '/tmp'
 
 # voxel size: z,y,x
 gvoxel_size = np.array([45,5,5])
 
 #%% znn forward
-gznn_batch_script_name = "batch_znn_forward.sh"
 gznn_znnpath = "/usr/people/jingpeng/seungmount/research/Jingpeng/01_ZNN/znn-release/"
+gznn_chann_fname = gznn_znnpath + "dataset/fish/data/batch91.image"
 gznn_netname = "W5_C10_P3_D2"
 gznn_fov = np.array([9,65,65])
 gznn_blocksize = np.array([20,20,20])
 gznn_bin = gznn_znnpath + "bin/znn"
+gznn_batch_script_name = "znn_batch_forward.sh"
 gznn_net_fname = gznn_znnpath + "networks/" + gznn_netname + ".spec"
 gznn_netpath = gznn_znnpath + "experiments/" + gznn_netname + "/network/"
+# temporary folder for znn, this folder should be unique for every node in AWS
 gznn_tmp = "/mnt/"
 gznn_threads = 7
 gznn_outsz = np.array([ 1, 100, 100 ])
-gznn_chann_fname = gznn_znnpath + "dataset/fish/data/batch92.image"
 
 #%% watershed chop
 # step: z,y,x
 gws_width = np.array([2000, 2000, 2000], dtype='uint32')
 # watershed parameters
-gws_bin_file = './src/quta/zi/watershed/main/bin/xxlws'
+gws_bin_file = gabspath + '/watershed/src/quta/zi/watershed/main/bin/xxlws'
 gws_high = 0.92
 gws_low = 0.3
 gws_dust = 400
@@ -37,7 +41,7 @@ gws_dust_low = 0.25
 gws_threads_num = 1
 
 # prepare the omnify data for omnifying
-gom_data_path = '../tmp/'
+gom_data_path = gtmp
 
 #%% watershed merge
 gws_merge_h5 = gom_data_path + "pywsmerge.Th-{}.Tl-{}.Ts-{}.Te-{}.h5".format(int(gws_high*1000), int(gws_low*1000), int(gws_dust), int(gws_dust_low*1000))
