e988a3ffec1231c1feb7bdc3688e15d2b56fe5ac
jingpengwu <jingpeng.wu@gmail.com>
2015-07-14 16:36:59 -0400

    multiple znn training instances in one cluster, share one EBS volume
    Former-commit-id: b57606596b10170fdaf1f8fd316ea51f28c8d0ac

--- aws_znn_train/persistent_spot_cluster.py
+++ aws_znn_train/persistent_spot_cluster.py
@@ -6,26 +6,21 @@ import threading
 #%% parameters
 conf_file = "~/.starcluster/config"
 
-# cluster tag or name
-tag = 'jingpeng3'
+# cluster name
+cluster_name = 'jingpeng'
 
-# cluster name and volume-id
-cnvi = {'jingpeng1':    'vol-5ba4b2b5',\
-        'jingpeng2':    'vol-df504131',\
-        'jingpeng3':    'vol-4f4051a1'}
+# node tag or name
+node_name = 'node1'
 
 # your bidding of spot instance
 spot_bid = 0.51
 
 # command
-cmd = 'cd /home/znn-release/; sh znn_train.sh'
+cmds = {'node1':'cd /home/znn-release/; sh znn_train1.sh',\
+        'node2':'cd /home/znn-release/; sh znn_train2.sh'}
 
 # instance type
 instance_type = 'c3.8xlarge'
-
-# since we want to run 1 master node as spot, so set it True.
-# set false for test and we will start a on-demand master node.
-force_spot_master = True 
 
 # if there are several cluster template in config file, you have to set the cluster id to a specific cluster template
 cluster_id = 0
@@ -37,9 +32,7 @@ sleep_interval = 1 * 60
 cfg = config.get_config( conf_file )
 cl = cfg.get_clusters()[ cluster_id ]
 cl.spot_bid = spot_bid
-cl.cluster_tag = tag
-cl.force_spot_master = force_spot_master
-cl.volumes['data']['volume_id'] = cnvi[tag]
+cl.cluster_tag = cluster_name
 cl.node_instance_type = instance_type
 
 #%% a thread to run
@@ -54,24 +47,16 @@ class ThreadRun(object):
         self.cl.start()
         cl.wait_for_cluster(msg='Waiting for cluster to come up...')
 
-#%% plugin
-from starcluster.clustersetup import ClusterSetup
-class RunZnn(ClusterSetup):
-    def __init__(self, cmd):
-        self._cmd = cmd
-    def run(self, nodes, master, user, user_shell, volumes):
-        master.ssh.execute( self._cmd )
-
-rz = RunZnn( cmd )
-
 #%% start the cluster
 print "constantly check whether this cluster is stopped or terminated."
 cid = 0
+f = open('log.txt','a+')
+f.write( "try to start a cluster with id: {}\n".format( cid ) )
 while True:
-    if (not cl.nodes) or cl.is_cluster_stopped() or cl.is_cluster_terminated():
+    # if cluster not started start the cluster
+    if cl.is_cluster_stopped() or cl.is_cluster_terminated():
         cid = cid + 1
-        f = open('log.txt','a+')
-        f.write( "try to start a cluster with id: {}\n".format( cid ) )
+        
         print "try to start a cluster with id: {}\n".format( cid )
         time.sleep(1)
 
@@ -84,19 +69,28 @@ while True:
             vol_id = cl.volumes['data']['volume_id']
             volume = cl.ec2.get_volume( vol_id )
             cl.ec2.wait_for_volume( volume, state='attached' )
-            
-            print "run plugin"
-            time.sleep(1*60)
-            cl.run_plugin(rz)
         except:
             print "running failed"
             time.sleep(1)
             pass
+    # if node not started, start the node
+    hasnode = False
+    for node in cl.nodes:
+        if node.alias == node_name:
+            mynode = node 
+            hasnode = True
+    if not hasnode:
+        mynode = cl.add_node( alias=node_name, spot_bid=spot_bid )
+        print "run plugin"
+        mynode.wait()
+        time.sleep(1*60)
+        mynode.ssh.execute( cmds[node_name] )
 
-        f.write('wait for cluster...\n')
-        time.sleep(1)
+    f.write('wait for cluster...\n')
+    time.sleep(1)
 
-        f.close()
     # sleep for a while
-    print "cluster is running, wait for {} secs to check.".format( sleep_interval )
+    print "node is running, wait for {} secs to check.".format( sleep_interval )
     time.sleep( sleep_interval )
+    
+f.close()
