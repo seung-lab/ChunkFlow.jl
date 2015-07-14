e0194ef381548faca8c67a77ec7e3a58ec1829ba
jingpengwu <jingpeng.wu@gmail.com>
2015-07-14 11:18:42 -0400

    fix some bugs
    Former-commit-id: 758e0163a4a1ad7eb2b80e57499d87d9eec6fb8f
    Former-commit-id: 0696556e6facb503ef7f560afee7078b7a0aa1ad

--- aws_znn_forward/znnout_to_h5.py
+++ aws_znn_forward/znnout_to_h5.py
@@ -16,8 +16,8 @@ Inputs:
 
 Main Outputs:
 
-    -Network Output HDF5 File ("{output_filename}")
-    -Channel Data HDF5 File ("channel_{output_filename}")
+    -Network Output HDF5 File ("{h5_aff_fname}")
+    -Channel Data HDF5 File ("channel_{h5_aff_fname}")
 
 Nicholas Turner, June 2015
 """
@@ -34,12 +34,9 @@ def write_channel_file(data, filename, dtype='float32'):
     '''Placing the cropped channel data within an hdf5 file'''
 
     f = h5py.File(filename, 'w')
-
     dset = f.create_dataset('/main', tuple(data.shape), dtype=dtype)
-
     #Saving a NORMALIZED version of the data (0<=d<=1)
     dset[:,:,:] = norm(data.astype(dtype))
-
     f.close()
 
 def write_affinity_file(data, filename, dtype='float32'):
@@ -47,16 +44,13 @@ def write_affinity_file(data, filename, dtype='float32'):
     specified by shape, and the number of volumes equal to the input data'''
 
     f = h5py.File(filename, 'w')
-
     dset = f.create_dataset('/main', tuple(data.shape), dtype=dtype)
-
     #Saving data
     dset[:, :,:,:] = data.astype(dtype)
-
     f.close()
 
 
-def main(net_output_filenames, image_filename, output_filename):
+def main(net_output_filenames, image_filename ):
 
     print "Importing data..."
     if len(net_output_filenames)==1 :
@@ -76,16 +70,13 @@ def main(net_output_filenames, image_filename, output_filename):
     #cropping the channel data to the 3d shape of the affinity graph
     cropped_image = crop(image, net_output.shape[-3:])
 
-    image_outname = 'channel_{}'.format(path.basename(output_filename))
-
     print "Writing network output file..."
-    write_affinity_file(net_output, output_filename)
+    write_affinity_file(net_output, gaffin_file)
     print "Writing image file..."
-    write_channel_file(cropped_image, image_outname)
+    write_channel_file(cropped_image, gchann_file)
 
 if __name__ == '__main__':
     #%% convert znn output to hdf5 file
-    fname = gznn_znnpath + "experiments/VeryDeep2HR_w65x9/output/out92."
+    fname = gznn_znnpath + "/experiments/VeryDeep2HR_w65x9/output/out92."
     gnet_out_fnames = (fname+"0", fname+"1", fname+"2")
-    gchann_fname = gznn_znnpath + "dataset/fish/data/batch92.image"
-    main(gnet_out_fnames, gchann_fname, gaffin_file)
+    main(gnet_out_fnames, gznn_chann_fname )
