5143ed9f6f24eaaafd20c240c3c5457fdd54e1c0
jingpengwu <jingpeng.wu@gmail.com>
2015-07-15 16:54:15 -0400

    first version of aws znn forward
    Former-commit-id: 782003ee819cb4f66fb22d1003cc21a2bb602a2b
    Former-commit-id: 163c88c55ffed99d28de43926bd7d6a90a2149d4

--- clean.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-from global_vars import *
-
-#%% clean, remove the temporary files
-shutil.rmtree( gtemp_file )
-shutil.rmtree( gznn_tmp )
-
-os.remove( gznn_batch_script_name )
