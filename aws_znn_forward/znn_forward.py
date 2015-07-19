ed728991b152b373fc93a5b04fd9f935c8047a5d
root <root@node001.(none)>
2015-07-19 22:29:13 +0000

    update
    Former-commit-id: df2f56b190f61ea6a98e1cd9a8d7349f964fbe19
    Former-commit-id: 9e8d4ca6215d1779fd3c5ba8693992853dfdd1a8

--- aws_znn_forward/znn_forward.py
+++ aws_znn_forward/znn_forward.py
@@ -184,7 +184,7 @@ def znn_forward_batch(z1,z2,y1,y2,x1,x2):
     the coordinate range is in the affinity map
     """
     # if we already have this file, directly return
-    if os.path.exists( gshared_tmp+'affin_X{}-{}_Y{}-{}_Z{}-{}.h5'.format(x1,x2,y1,y2,z1,z2) ):
+    if os.path.exists( gshared_tmp+'affin_Z{}-{}_Y{}-{}_X{}-{}.h5'.format(z1,z2,y1,y2,x1,x2) ):
         return
     # clear and prepare local temporal folder
     if os.path.exists(gtmp):
@@ -192,14 +192,14 @@ def znn_forward_batch(z1,z2,y1,y2,x1,x2):
     os.mkdir(gtmp)
     
     # read the cube
-    f = h5py.File(gshared_tmp+'chann_X{}-{}_Y{}-{}_Z{}-{}.h5'.format(x1,x2,y1,y2,z1,z2))
+    f = h5py.File(gshared_tmp+'chann_Z{}-{}_Y{}-{}_X{}-{}.h5'.format(z1,z2,y1,y2,x1,x2))
     cv = np.asarray(f['/main'])
     f.close()
     
     # run forward for this cube
     affv = znn_forward(cv)
     
-    f = h5py.File( gshared_tmp+'affin_X{}-{}_Y{}-{}_Z{}-{}.h5'.format(x1,x2,y1,y2,z1,z2) )
+    f = h5py.File( gshared_tmp+'affin_Z{}-{}_Y{}-{}_X{}-{}.h5'.format(z1,z2,y1,y2,x1,x2) )
     f['/main'] = affv
     f.close()
 
