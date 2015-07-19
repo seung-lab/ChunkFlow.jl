c4870d6b20cd4603c217f7e2e1aa6673d8b26bd4
root <root@node001.(none)>
2015-07-19 19:00:54 +0000

    fix affin cube bug
    Former-commit-id: 623287633b863b8a4c8edd5f02432bf35220f09b
    Former-commit-id: 477a07dafb08957354e543aefb0cbdb59a05ef4d

--- aws_znn_forward/znn_chop.py
+++ aws_znn_forward/znn_chop.py
@@ -13,22 +13,27 @@ import emirt
 
 def prepare_cube(z1,z2,y1,y2,x1,x2):
     # extract cube in large volume
+    # get affinity size
+    from znn_merge import get_fov
+    fov = get_fov()
+    offset = (fov - 1)/2
+
     f = h5py.File(gznn_raw_chann_fname)
-    vol = np.asarray( f['/main'][z1:z2,y1:y2,x1:x2] )
+    vol = np.asarray( f['/main'][z1:z2+2*offset[0],y1:y2+2*offset[1],x1:x2+2*offset[2]] )
     f.close
     
     # save the cube
-    f = h5py.File( gshared_tmp+'cube_X{}-{}_Y{}-{}_Z{}-{}.h5'.format(x1,x2,y1,y2,z1,z2) )
+    f = h5py.File( gshared_tmp+'chann_X{}-{}_Y{}-{}_Z{}-{}.h5'.format(x1,x2,y1,y2,z1,z2) )
     f.create_dataset('/main', data=vol, dtype='double')
     f.close()
 
 def prepare_batch_script():
     print "prepare batch script..."
     # get the volume shape
-    fa = h5py.File( gaffin_file )
-    aff_size = np.asarray(fa['/main'].shape)
-    print aff_size
-    fa.close()
+    fc = h5py.File( gchann_file )
+    size2 = np.asarray(fc['/main'].shape)
+    print size2
+    fc.close()
     
     if shutil.os.path.exists( gznn_batch_script_name ):
         shutil.os.remove( gznn_batch_script_name )
@@ -38,15 +43,14 @@ def prepare_batch_script():
     # locate cubes and prepare file
     # list of cube coordinates
     cube_coords=[]
-    for z in xrange(0, aff_size[1], gznn_blocksize[0]):
-        for y in xrange(0, aff_size[2], gznn_blocksize[1]):
-            for x in xrange(0, aff_size[3], gznn_blocksize[2]):
-                z1, z2 = z,  min( aff_size[1], z+gznn_blocksize[0] )
-                y1, y2 = y,  min( aff_size[2], y+gznn_blocksize[1] )
-                x1, x2 = x,  min( aff_size[3], x+gznn_blocksize[2] )
+    for z in xrange(0, size2[0], gznn_blocksize[0]):
+        for y in xrange(0, size2[1], gznn_blocksize[1]):
+            for x in xrange(0, size2[2], gznn_blocksize[2]):
+                z1, z2 = z,  min( size2[0], z+gznn_blocksize[0] )
+                y1, y2 = y,  min( size2[1], y+gznn_blocksize[1] )
+                x1, x2 = x,  min( size2[2], x+gznn_blocksize[2] )
                 cube_coords.append((z1,z2,y1,y2,x1,x2))
-                f.write("python " + gabspath + "aws_znn_forward/znn_forward.py {} {} {} {} {} {}\n".format( \                                                            
-                               z1, z2,y1,y2,x1,x2))
+                f.write("python " + gabspath + "aws_znn_forward/znn_forward.py {} {} {} {} {} {}\n".format(z1, z2,y1,y2,x1,x2))
                 prepare_cube(z1,z2,y1,y2,x1,x2)
     f.close()
     
@@ -81,23 +85,28 @@ def prepare_h5():
     fc = h5py.File( gznn_raw_chann_fname )
     raw_chann = fc['/main']
     sz_chann = raw_chann.shape
-    
+    # get croped channel volume size
+    from znn_merge import get_fov
+    fov = get_fov()
+    offset = (fov - 1)/2
+    crop_shape = sz_chann - fov+1
+ 
     # create the channel hdf5 file
     if shutil.os.path.exists( gchann_file ):
             shutil.os.remove( gchann_file )
     fc2 = h5py.File( gchann_file )
-    fc2.create_dataset('/main', shape=sz_affin[1:4], dtype="float32" )
+    fc2.create_dataset('/main', shape=crop_shape, dtype="float32" )
     chann2 = fc2['/main']
     # in case the channel data is huge, copy layer by layer
-    for z in xrange( sz_affin[1] ):
+    for z in xrange( crop_shape[0] ):
         sec = raw_chann[z+offset[0], offset[1]:-offset[1], offset[2]:-offset[2]]
         # normalize for omni
-        chann2[z,:,:] = emirt.volume_util.norm( sec )
+        chann2[z,:,:] = emirt.volume_util.norm( np.asarray(sec) )
         
     fc.close()
     fc2.close()
     # remove the original file (to-do)
-    os.remove()
+    # os.remove( gznn_raw_chann_fname )
 
 def znn_chop():
     # clear and prepare shared temporary folder
