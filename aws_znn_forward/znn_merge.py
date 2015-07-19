c3302c890ae4564418ac9d924172e28be0bf85cf
JINGPENG <jingpeng.wu@gmail.com>
2015-07-19 14:03:42 -0400

    add chop and merge file
    Former-commit-id: 8df5dfa71f4229c4d8a0fc40b2c1e534421253fa
    Former-commit-id: a37b54f54f5d541d5bd403f626b9d46cfda3b068

---
+++ aws_znn_forward/znn_merge.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 19 10:22:38 2015
+
+@author: jingpeng
+"""
+from global_vars import *
+
+def get_fov():
+    if len(gznn_fovs)==1:
+        return gznn_fovs[0]
+    elif len(gznn_fovs)==2:
+        return gznn_fovs[0]+gznn_fovs[1]-1
+    else:
+        raise NameError("do not support this FoV parameter!")
+
+def znn_merge():
+    """
+    merge the cubes in an affinity h5 file
+    """
+    # prepare affinity file
+    fov = get_fov()
+    offset = (fov - 1)/2
+    sz_affin = sz_chann - 2*offset
+    sz_affin = np.hstack((3,sz_affin))
+    # create the affinity hdf5 file
+    if shutil.os.path.exists( gaffin_file ):
+            shutil.os.remove( gaffin_file )
+    fa = h5py.File( gaffin_file )
+    fa.create_dataset('/main', shape=sz_affin, dtype='float32', chunks=True, compression="gzip")
+    fa.close()
+    
+    # merge the cubes in affinity file
+    # read coordinates
+    f = h5py.File(gshared_tmp + 'cube_coordinates.h5')
+    coords = list( f['/main'] )
+    f.close()
+    # merge the cubes
+    for c in coords:
+        z1,z2,y1,y2,x1,x2 = c
+        # read the cube
+        fc = h5py.File( gshared_tmp+'cube_X{}-{}_Y{}-{}_Z{}-{}.h5'.format(x1,x2,y1,y2,z1,z2) )
+        vol = np.asarray( fc['/main'] )
+        fc.close()
+        # save in global affinity file
+        fa = h5py.File( gaffin_file )
+        fa['/main'][:,z1:z2,y1:y2,x1,x2] = vol
+        fa.close()
+        
