3ca0ee5d376a1504b03447473e453388e4f3617e
JINGPENG <jingpeng.wu@gmail.com>
2015-07-19 11:21:34 -0400

    the first whole pipeline version
    Former-commit-id: e1078a91d1c84dbc2fc2f54f80e447a2fe7a2a23

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ import time
 import os
 from global_vars import *
 #%% run parallel znn forward
-
+from aws_znn_forward.znn_chop import znn_chop
+znn_chop()
+os.system('qsub -V -b y -cwd '+ gznn_batch_script_name )
+from aws_znn_forward.znn_merge import znn_merge
+znn_merge()
 
 #%% watershed chop
 from watershed.watershed_chop import watershed_chop
