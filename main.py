fedcee38338c761cdcd1f8aacc2cb6ed59088b75
root <root@node001.(none)>
2015-07-20 02:37:44 +0000

    fix two stage isaff parameter bug
    Former-commit-id: 8818c03b97823c35dae9850f5b3d39325c566fc4
    Former-commit-id: d7021d33d2b5357dfe042d62fcd1be5149eb6dba

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ Jingpeng Wu <jingpeng.wu@gmail.com>, 2015
 import time
 import os
 from global_vars import *
-#%% run parallel znn forward
-from aws_znn_forward.znn_chop import znn_chop
-#znn_chop()
-#os.system('qsub -V -b y -cwd '+ gznn_batch_script_name )
-#os.system('sh '+ gznn_batch_script_name)
-from aws_znn_forward.znn_merge import znn_merge
-znn_merge()
 
 #%% watershed chop
 from watershed.watershed_chop import watershed_chop
