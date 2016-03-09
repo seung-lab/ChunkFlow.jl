5e284df964b1cdf3da63a4067c71aac14a684068
jingpengwu <jingpeng.wu@gmail.com>
2016-03-08 21:57:10 -0500

    directly use tif image stack as raw image rather than hdf5
    Former-commit-id: d0246dabb965d5bc508d2bd63205a76589315615
    Former-commit-id: c54513e75e4f454852c947717ce6ffe8d3108ebc

--- julia/segm2omprj.jl
+++ julia/segm2omprj.jl
@@ -1,8 +1,22 @@
+export segm2omprj
+
 function segm2omprj(ombin, fimg, fsegm, vs=[4,4,40], fomprj="/tmp/tmp.omni")
+    fimgh5 = fimg
+    if contains(fimg, ".tif")
+        # transform tif image to hdf5
+        using EMIRT
+        img = imread(fimg)
+        fimgh5 = replace(fimg, ".tiff", ".h5")
+        fimgh5 = replace(fimg, ".tif", ".h5")
+        if isfile(fimgh5)
+            rm(fimgh5)
+        end
+        imsave(img, fimgh5)
+    end
     # prepare the cmd file for omnification
     # make omnify command file
     cmd = """create:$(fomprj)
-    loadHDF5chann:$(fimg)
+    loadHDF5chann:$(fimgh5)
     setChanResolution:1,$(vs[1]),$(vs[2]),$(vs[3])
     setChanAbsOffset:,1,0,0,0
     loadHDF5seg:$(fsegm)
