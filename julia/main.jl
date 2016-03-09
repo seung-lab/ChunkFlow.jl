399d19a072714053495cfac9b5d013f3d5be821f
jingpengwu <jingpeng.wu@gmail.com>
2016-03-09 15:56:56 -0500

    add command lineargument
    Former-commit-id: 26a5a9c7aa62c5c3350ba8e1f6c6e3a9082b5f9b
    Former-commit-id: 2e67ccc08f31821c31f687424595732e5cc3bb93

--- julia/main.jl
+++ julia/main.jl
@@ -5,38 +5,50 @@ include("segm2omprj.jl")
 include("zforward.jl")
 
 # parse the config file
-fconf = "params.cfg"
+if length(ARGS)==0
+    fconf = "params.cfg"
+elseif length(ARGS)==1
+    fconf = ARGS[1]
+else
+    error("too many commandline arguments")
+end
+
 pd = configparser(fconf)
 
 # get affinity map
 faffs = pd["gn"]["faffs"]
-if !isfile(faffs) || pd["znn"]["is_overwrite"]
-    println("run forward path to create one...")
-    if isfile(faffs) && pd["znn"]["is_overwrite"]
-        rm(faffs)
+if pd["znn"]["is_znn"]
+    if !isfile(faffs) || pd["znn"]["is_overwrite"]
+        println("run forward path to create one...")
+        if isfile(faffs) && pd["znn"]["is_overwrite"]
+            rm(faffs)
+        end
+        zforward(faffs, pd["gn"]["tmp_dir"], pd["gn"]["fimg"], pd["znn"]["dir"], pd["znn"]["fnet_specs"][1], pd["znn"]["fnets"][1], pd["znn"]["outszs"][1:3], pd["znn"]["fnet_specs"][2], pd["znn"]["fnets"][2], pd["znn"]["outszs"][4:6], pd["znn"]["is_stdio"])
     end
-    zforward(faffs, pd["gn"]["tmp_dir"], pd["gn"]["fimg"], pd["znn"]["dir"], pd["znn"]["fnet_specs"][1], pd["znn"]["fnets"][1], pd["znn"]["outszs"][1:3], pd["znn"]["fnet_specs"][2], pd["znn"]["fnets"][2], pd["znn"]["outszs"][4:6], pd["znn"]["is_stdio"])
 end
-
-# read affinity map
-println("reading affinity map...")
-affs = h5read(faffs, "/main")
 
 # watershed, affs to segm
+if pd["ws"]["is_watershed"]
+    # read affinity map
+    println("reading affinity map...")
+    affs = h5read(faffs, "/main")
 
-# watershed
-# exchange x and z channel
-if pd["ws"]["is_exchange_affs_xz"]
-    exchangeaffsxz!(affs)
-end
-# remap to uniform distribution
-if pd["ws"]["is_remap"]
-    affs2uniform!(affs)
-end
+    # watershed
+    # exchange x and z channel
+    if pd["ws"]["is_exchange_affs_xz"]
+        exchangeaffsxz!(affs)
+    end
+    # remap to uniform distribution
+    if pd["ws"]["is_remap"]
+        affs2uniform!(affs)
+    end
 
-seg, dend, dendValues = affs2segm(affs, pd["ws"]["low"], pd["ws"]["high"])
-# save seg and mst
-save_segm(pd["gn"]["fsegm"], seg, dend, dendValues)
+    seg, dend, dendValues = affs2segm(affs, pd["ws"]["low"], pd["ws"]["high"])
+    # save seg and mst
+    save_segm(pd["gn"]["fsegm"], seg, dend, dendValues)
+end
 
 # omnification
-segm2omprj(pd["omni"]["ombin"], pd["gn"]["fimg"], pd["gn"]["fsegm"], pd["gn"]["voxel_size"], pd["omni"]["fomprj"])
+if pd["omni"]["is_omni"]
+    segm2omprj(pd["omni"]["ombin"], pd["gn"]["fimg"], pd["gn"]["fsegm"], pd["gn"]["voxel_size"], pd["omni"]["fomprj"])
+end
