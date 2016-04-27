8eae16a2251d362143d2bbfa01b133e15f30ec30
jingpengwu <jingpeng.wu@gmail.com>
2016-04-27 18:38:55 -0400

    clean up the code, but this version do not work yet
    Former-commit-id: 76c3e7756560b1ab608bb02878ed5829810110fc

--- zforward.jl
+++ zforward.jl
@@ -4,10 +4,10 @@ export zforward
 typealias Tarrfile Any
 #Union{AbstractString, Array}
 
-function arr2path(tmp_dir, inps::Dict, key::AbstractString)
+function arr2path(tmpdir, inps::Dict, key::AbstractString)
     # automatic input transformation
     if typeof( inps[key] ) <: Array
-        farr = joinpath(tmp_dir, "$(key).h5")
+        farr = joinpath(tmpdir, "$(key).h5")
         imsave(inps[key], farr)
         return farr
     elseif typeof( inps[key] ) <: AbstractString
@@ -18,8 +18,8 @@ end
 """
 create temporal dataset specification file
 """
-function create_dataset_spec(tmp_dir::AbstractString, inps::Dict)
-    fimg = arr2path(tmp_dir, inps, "img")
+function create_dataset_spec(tmpdir::AbstractString, inps::Dict)
+    fimg = arr2path(tmpdir, inps, "img")
     dspec = """
     [image1]
     fnames = $(fimg)
@@ -38,7 +38,7 @@ function create_dataset_spec(tmp_dir::AbstractString, inps::Dict)
         is_auto_crop = yes
 
         [image10]
-        fnames = $(tmp_dir)/out_sample1_output_0.tif
+        fnames = $(tmpdir)/out_sample1_output_0.tif
         pp_types = symetric_rescale
         is_auto_crop = yes
         fmasks =
@@ -50,66 +50,79 @@ function create_dataset_spec(tmp_dir::AbstractString, inps::Dict)
     end
 
     # write the spec file
-    f = open("$(tmp_dir)/dataset.spec", "w")
+    f = open("$(tmpdir)/dataset.spec", "w")
     write(f, dspec)
     close(f)
 end
 
-function create_config(prms, inps)
-    tmp_dir = prms["tmp_dir"]
+function create_config(d, inps)
+    tmpdir = d["tmpdir"]
 
     # standard IO or not
-    if prms["is_stdio"]
+    if d["is_stdio"]
         stdio = "yes"
     else
         stdio = "no"
     end
-    outsz = prms["outsz"]
+    outsz = d["outsz"]
     # configuration string
     conf="""
     [parameters]
-    fnet_spec = $(prms["fnet_spec"])
+    fnet_spec = $(d["fnet_spec"])
     cost_fn = auto
-    fdata_spec = $(tmp_dir)/dataset.spec
+    fdata_spec = $(tmpdir)/dataset.spec
     num_threads = 0
     dtype = float32
-    out_type = $(prms["out_type"])
+    out_type = $(d["out_type"])
     forward_range = 1
     is_bd_mirror = yes
-    forward_net = $(prms["fnet"])
+    forward_net = $(d["fnet"])
     is_stdio = $(stdio)
     forward_conv_mode = fft
     forward_outsz = $(outsz[1]),$(outsz[2]),$(outsz[3])
-    output_prefix = $(tmp_dir)/out
+    output_prefix = $(tmpdir)/out
     """
     # write the config file
-    f = open("$(tmp_dir)/forward.cfg", "w")
+    f = open("$(tmpdir)/forward.cfg", "w")
     write(f, conf)
     close(f)
 end
 
-function zforward( prms::Dict{AbstractString, Any}, inps::Dict )
-    tmp_dir = prms["tmp_dir"]
+function zforward( d::Dict{AbstractString, Any} )
+    if !d["is_znn"]
+        return
+    end
+
+    if isfile(d["faff"])
+        println("remove existing affinity file...")
+        rm(d["faff"])
+    end
+
+    println("znn forward pass...")
+
+    inps = Dict( "img"=> d["fimg"] )
+
+    tmpdir = d["tmpdir"]
     # create dataset specification file
-    create_dataset_spec( tmp_dir, inps )
+    create_dataset_spec( tmpdir, inps )
     # create forward pass stage 1 configuration file
-    create_config( prms, inps )
+    create_config( d, inps )
     # current path
     cp = pwd()
     # run recursive forward pass
-    cd("$(prms["dir"])/python")
-    run(`python forward.py -c $(tmp_dir)/forward.cfg -n $(prms["fnet"]) -r 1`)
+    cd("$(d["dir"])/python")
+    run(`python forward.py -c $(tmpdir)/forward.cfg -n $(d["fnet"]) -r 1`)
     cd(cp)
     # move the output affinity to destination
-    outfname = joinpath(tmp_dir, "out_sample1_output.h5")
-    if contains(prms["out_type"], "aff")
-        fout = joinpath(tmp_dir, "aff.h5")
+    outfname = joinpath(tmpdir, "out_sample1_output.h5")
+    if contains(d["out_type"], "aff")
+        fout = joinpath(tmpdir, "aff.h5")
         ret = Dict( "aff"=>fout )
     else
-        fout = joinpath(tmp_dir, "bdr.h5")
+        fout = joinpath(tmpdir, "bdr.h5")
         ret = Dict( "bdr"=>fout )
     end
-    if outfname != prms["faff"]
+    if outfname != d["faff"]
         mv(outfname, fout, remove_destination=true)
     end
     return ret
