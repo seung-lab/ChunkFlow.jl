c3b105e2adcbc7b003a0eedd0b7997269fa81713
jingpengwu <jingpeng.wu@gmail.com>
2016-05-03 12:24:07 -0400

    add original aff option; update lambda function;
    Former-commit-id: 6f2c5bb2422d35b0be3e1ad142043ecf4bfa9258

--- lambda_function.py
+++ lambda_function.py
@@ -1,28 +1,12 @@
 import boto3
-import json
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
-    # download the config file from S3 to local lambda server
-    fconf = '/tmp/config.cfg'
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key'] 
-        s3.download_file(bucket, key, fconf)
-    
-    # read the config file
-    f = open(fconf, 'r')
-    conf = f.read()
-    f.close()
-    if conf=="":
-        return
-    
-    # propose the task to aws Simple Queue Service
-    sqs = boto3.resource('sqs')
-    queue = sqs.get_queue_by_name(QueueName='spipe-tasks')
-    # send the configuration file as a message in SQS
-    queue.send_message(MessageBody=json.dumps(conf))
-    
+    # find the triger file in bucket
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    key = record['s3']['object']['key'] 
+        
     # my bash code to execute 
     myscript="""#!/bin/bash
 export HOME=/root/
@@ -34,26 +18,32 @@ export JULIA_LOAD_PATH=$JULIA_LOAD_PATH:/usr/local/share/julia/site/v0.4/Agglome
 export JULIA_LOAD_PATH=$JULIA_LOAD_PATH:/usr/local/share/julia/site/v0.4/Agglomerator/src/Features
 export JULIA_LOAD_PATH=$JULIA_LOAD_PATH:/usr/local/share/julia/site/v0.4/Agglomerator/src/Visualization
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/boost/lib
+
+cd /opt/spipe
+git checkout master
+git pull
+
+cd /usr/local/share/julia/v0.4/EMIRT
+git checkout master
+git pull
+
 mkfs -t ext4 /dev/xvde                                                                                                 
 mount /dev/xvde /tmp
 rm -rf /tmp/*
-cd /opt/spipe
-git pull
-#cd /opt/znn-release
-#git pull
-julia /opt/spipe/main.jl
-"""
+
+julia /opt/spipe/main.jl s3://{}/{}
+""".format(bucket, key)
     
-    # launch a master node to handle this task
+    # launch a node to handle this task
     ec2 = boto3.client('ec2')
     ec2.run_instances(
         DryRun = False,
-        ImageId = 'ami-a6c5d9cc',
+        ImageId = 'ami-8d08ece0',
         MinCount = 1 ,
         MaxCount = 1 ,
         KeyName = 'jpwu_workstation',
         UserData = myscript,
-        InstanceType = 'c4.8xlarge',
+        InstanceType = 'cr3.8xlarge',
         InstanceInitiatedShutdownBehavior='terminate', # 'stop' | 'terminate',
         BlockDeviceMappings = [
             {
