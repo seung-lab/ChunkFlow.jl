2e41ed16473bfde163d2beca956845288f003cff
jingpengwu <jingpeng.wu@gmail.com>
2016-05-04 21:14:38 -0400

    submit a bunch of taskes
    Former-commit-id: ebb560bd235527a70aa903d01ec5fbf8a941211a

--- taskproducer.jl
+++ taskproducer.jl
@@ -11,10 +11,27 @@ pd = configparser(task)
 @assert iss3( pd["gn"]["fimg"] )
 
 # get list of files, no folders
-lf = s3_list_objects(env, pd["gn"]["fimg"])
-for f in lf
-    # make a new task with this specific file name
-
-    # send the task to SQS queue
+bkt, keylst = s3_list_objects(env, pd["gn"]["fimg"])
+@assert length(keylst)>0
+if length(keylst)==1
+    # directly send the task to SQS queue
     sendSQSmessage(env, "spipe-tasks", task)
+else
+    lines = split(task, "\n")
+    for key in keylst
+        # a new task
+        newtask = ""
+        # make a new task with this specific file name
+        for i in length(lines)
+            line = lines[i]
+            line = replace(line, " ", "")*"\n"
+            if ismatch(r"^fimg", line)
+                line = "fimg=s3://"*joinpath(bkt, key)*"\n"
+            end
+            # add the line to new task
+            newtask *= line
+        end
+        # send the task to SQS queue
+        sendSQSmessage(env, "spipe-tasks", newtask)
+    end
 end
