644216d7622783b720c3ece678bf7bafa5e85acf
jingpengwu <jingpeng.wu@gmail.com>
2016-05-04 23:59:44 -0400

    batch submitting of tasks works
    Former-commit-id: c764da56d6314c9920acc90ed7e8dae0da9131ad

--- taskproducer.jl
+++ taskproducer.jl
@@ -6,32 +6,39 @@ const global env = build_env()
 
 # read task config file
 task = readall(ARGS[1])
-
 pd = configparser(task)
 @assert iss3( pd["gn"]["fimg"] )
+@show pd
 
 # get list of files, no folders
 bkt, keylst = s3_list_objects(env, pd["gn"]["fimg"])
+@show bkt
+@show keylst
 @assert length(keylst)>0
+
 if length(keylst)==1
     # directly send the task to SQS queue
     sendSQSmessage(env, "spipe-tasks", task)
 else
     lines = split(task, "\n")
+    @show lines
     for key in keylst
         # a new task
         newtask = ""
         # make a new task with this specific file name
-        for i in length(lines)
+        for i in 1:length(lines)
             line = lines[i]
-            line = replace(line, " ", "")*"\n"
+            @show line
+            line = replace(line, " ", "")
             if ismatch(r"^fimg", line)
-                line = "fimg=s3://"*joinpath(bkt, key)*"\n"
+                line = string("fimg=s3://", joinpath(bkt, key))
             end
             # add the line to new task
-            newtask *= line
+            newtask = string(newtask, line, "\n")
+            @show newtask
         end
         # send the task to SQS queue
+        @show newtask
         sendSQSmessage(env, "spipe-tasks", newtask)
     end
 end
