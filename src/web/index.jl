a1a013d65a2eb00ad02a393ce91e29c9afae630a
jingpengwu <jingpeng.wu@gmail.com>
2016-05-17 13:38:10 -0400

    update
    Former-commit-id: c227559f50d5c37b87c95001958e94b283f4fc55

--- src/web/index.jl
+++ src/web/index.jl
@@ -7,11 +7,11 @@ function wiretabs()
     tabbar = tabs([hbox(icon("autorenew"), hskip(1em), "Train"),
                    hbox(icon("trending-down"), hskip(1em), "LearningCurve"),
                    hbox(icon("forward"), hskip(1em), "Inference"),
-                   hbox(icon("dashboard"), hskip(1em), "Watershed"),
+                   hbox(icon("dashboard"), hskip(1em), "Segmentation"),
                    hbox(icon("assessment"), hskip(1em), "Evaluate"),
                    hbox(icon("polymer"), hskip(1em), "Pipeline"),
                    hbox(icon("help"), hskip(1em), "Help")] )
-    tabcontents = pages([ "training", plotcurve(), "forward pass", "watershed", evaluate(), "Pipeline", "help"])
+    tabcontents = pages([ "training", plotcurve(), "forward pass", "segmentation", evaluate(), "Pipeline", "help"])
 
     t, p = wire( tabbar, tabcontents, :tabschannel, :selected)
 
