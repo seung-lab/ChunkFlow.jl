e50d34110be0a5454ca1d8af42ad56e337ee1f04
jingpengwu <jingpeng.wu@gmail.com>
2016-05-17 11:18:17 -0400

    move znn web front end; reorganize code;
    Former-commit-id: 36b13a48ce2a761e301ce858e3cb3c25fc2253e7

---
+++ src/web/plotcurve.jl
@@ -0,0 +1,90 @@
+using Gadfly
+using Distributions
+using HDF5
+import Escher: Sampler
+
+function get_learning_curve(fname::AbstractString)
+    if contains(fname, "s3://")
+        # local file name
+        lcfname = "/tmp/net_current.h5"
+        # download from  AWS S3
+        run(`aws s3 cp $(fname) $(lcfname)`)
+        # rename fname to local file name
+        fname = lcfname
+    end
+    curve = Dict{ASCIIString, Dict{ASCIIString,Vector}}()
+    if isfile(fname)
+        curve["train"] = Dict{ASCIIString, Vector}()
+        curve["test"]  = Dict{ASCIIString, Vector}()
+
+        curve["train"]["it"]  = h5read(fname, "/processing/znn/train/statistics/train/it")
+        curve["train"]["err"] = h5read(fname, "/processing/znn/train/statistics/train/err")
+        curve["train"]["cls"] = h5read(fname, "/processing/znn/train/statistics/train/cls")
+        curve["test"]["it"]   = h5read(fname, "/processing/znn/train/statistics/test/it")
+        curve["test"]["err"]  = h5read(fname, "/processing/znn/train/statistics/test/err")
+        curve["test"]["cls"]  = h5read(fname, "/processing/znn/train/statistics/test/cls")
+    end
+    return curve
+end
+
+function tile_learning_curve(curve::Dict)
+    if length( keys(curve) ) == 0
+        return ""
+    else
+        return vbox(
+                    md"## Learning Curve of Cost",
+                    drawing(8Gadfly.inch, 4Gadfly.inch,
+                            plot(layer(x=curve["train"]["it"]/1000, y=curve["train"]["err"],
+                                       Geom.line, Theme(default_color=color("blue"))),
+                                 layer(x=curve["test"]["it"] /1000, y=curve["test"]["err"],
+                                       Geom.line, Theme(default_color=color("red"))),
+                                 Guide.xlabel("Iteration (K)"), Guide.ylabel("Cost"))),
+        md"## Learning Curve of Pixel Error",
+        drawing(8Gadfly.inch, 4Gadfly.inch,
+                plot(layer(x=curve["train"]["it"]/1000, y=curve["train"]["cls"],
+                           Geom.line, Theme(default_color=color("blue"))),
+                     layer(x=curve["test"]["it"] /1000, y=curve["test"]["cls"],
+                           Geom.line, Theme(default_color=color("red"))),
+                     Guide.xlabel("Iteration (K)"), Guide.ylabel("Pixel Error"))) #,
+        ) |> Escher.pad(2em)
+    end
+end
+
+function tile_learning_curve(fname::AbstractString)
+    curve = get_learning_curve(fname)
+    return tile_learning_curve(curve)
+end
+
+"""
+the form tile to provide learning curve plotting tile
+"""
+function tile_form_network_file(inp::Signal, s::Sampler)
+    return vbox(
+                h1("Choose your network file"),
+                watch!(s, :fname, textinput("/tmp/net_current.h5", label="network file")),
+                trigger!(s, :plot, button("Plot Learning Curve"))
+                ) |> maxwidth(400px)
+end
+
+"""
+get learning curve plotting tile
+`Parameters`:
+inp: input
+s: sampler
+
+`Return`:
+ret: learning curve plotting tile
+"""
+function plotcurve()
+    inp = Signal(Dict())
+    s = Escher.sampler()
+    form = tile_form_network_file(inp, s)
+    ret = consume(inp) do dict
+        vbox(
+                intent(s, form) >>> inp,
+                vskip(2em),
+                tile_learning_curve(get(dict, :fname, ""))
+                ) |> Escher.pad(2em)
+    end
+    return ret
+end
