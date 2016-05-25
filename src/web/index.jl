698c8d37645727c6a15c8cc25755bec7f01e215f
jingpengwu <jingpeng.wu@gmail.com>
2016-05-25 16:54:52 -0400

    update lambda function to use either spot or on-demand instance; use the same watched input key in web-frontend
    Former-commit-id: de7792262f2cf08ac6f3ed54d692a84d1990dbcf
    Former-commit-id: 7a6d089300c05d86c21ce17b79bc8641a9b6686a

--- src/web/index.jl
+++ src/web/index.jl
@@ -1,13 +1,15 @@
 import Escher:  Sampler
 
 include("plotcurve.jl")
-include("evaluate.jl")
 include("segmentation.jl")
+include("evaluate.jl")
+
 
 main(window) = begin
     push!(window.assets, "layout2")
     push!(window.assets, "icons")
     push!(window.assets, "widgets")
+
     tabbar = tabs([hbox(icon("autorenew"),     hskip(1em), "Train"),
                    hbox(icon("trending-down"), hskip(1em), "LearningCurve"),
                    hbox(icon("forward"),       hskip(1em), "Inference"),
@@ -15,11 +17,11 @@ main(window) = begin
                    hbox(icon("assessment"),    hskip(1em), "Evaluate"),
                    hbox(icon("polymer"),       hskip(1em), "Pipeline"),
                    hbox(icon("help"),          hskip(1em), "Help")] )
-    tabcontents = pages([ "training", plotcurve(), "forward pass", "segmentation()", "evaluate()", "Pipeline", "help"])
+    tabcontents = pages([ "training"; plotcurve(); "forward pass"; segmentation(); segmentation(); "Pipeline"; "help"])
 
     t, p = wire( tabbar, tabcontents, :tabschannel, :selected)
 
-    vbox(toolbar(["ZNN @", iconbutton("cloud"), flex()]),
+    vbox(toolbar(["Seunglab Pipeline @", iconbutton("cloud"), flex()]),
          Escher.pad(1em, t),
          Escher.pad(1em, p))
 end
