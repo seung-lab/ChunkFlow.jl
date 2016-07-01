062766ecedd2a35026827fe277ff16a6b2c77a6f
jingpengwu <jingpeng.wu@gmail.com>
2016-07-01 16:35:06 -0400

    use symbol as dictionary key to enhance efficiency
    Former-commit-id: e6bb10def2d59a561cac43535ff7acefed5a8a46
    Former-commit-id: 16cc171fc398c9ac730023678da2c8ea1ef2d672

--- src/core/aff2sgm.jl
+++ src/core/aff2sgm.jl
@@ -27,44 +27,57 @@ end
 """
 transform affinity map to segmentation with mst
 """
-function aff2sgm(d::Dict{AbstractString, Any})
-    if contains(d["node_switch"], "off")
+function aff2sgm(d::Dict{Symbol, Any})
+    if contains(d[:node_switch], "off")
         return
     end
     # read affinity map
     print("reading affinity map...")
-    aff = h5read(d["faff"], "/main")
+    aff = h5read(d[:faff], "/main")
     println("done!")
 
     # watershed
     # exchange x and z channel
-    if d["is_exchange_aff_xz"]
+    if d[:is_exchange_aff_xz]
         exchangeaffxz!(aff)
     end
 
-    if contains(d["remap_type"], "uniform")
+    if contains(d[:remap_type], "uniform")
         # remap the affinity to uniform distribution, will do sorting
         unfaff = aff2uniform(aff)
-        sgm = aff2sgm(unfaff, d["low"], d["high"], d["thresholds"], d["dustsize"])
-    elseif contains(d["remap_type"], "percent")
+        sgm = aff2sgm(unfaff, d[:low], d[:high], d[:thresholds], d[:dustsize])
+    elseif contains(d[:remap_type], "percent")
         # use percentage threshold
         e, count = hist(aff[:], 10000)
-        low  = percent2thd(e, count, d["low"])
-        high = percent2thd(e, count, d["high"])
+        low  = percent2thd(e, count, d[:low])
+        high = percent2thd(e, count, d[:high])
         thds = Vector{Tuple}()
-        for tp in d["thresholds"]
+        for tp in d[:thresholds]
             push!(thds, tuple(tp[1], percent2thd(e, count, tp[2])))
         end
-        sgm = aff2sgm(aff, low, high, thds, d["dustsize"])
+        sgm = aff2sgm(aff, low, high, thds, d[:dustsize])
     else
         # use absolute threshold
         sgm = aff2sgm(aff, d)
     end
 
+    if haskey(d, :cropsize) && any( d[:cropsize] )>0
+        @assert d[:agg_mode]!=nothing
+        # total size
+        tsz = size(sgm.seg)
+        # crop size
+        csz = d[:cropsize]
+        seg = sgm.seg[  csz[1]+1 : tsz[1]-csz[1],
+                            csz[2]+1 : tsz[2]-csz[2],
+                            csz[3]+1 : tsz[3]-csz[3]]
+    else
+        seg = sgm.seg
+    end
+
     # aggromeration
-    if d["agg_mode"]=="mean"
+    if contains( d[:agg_mode], "mean")
         println("mean affinity agglomeration...")
-        if contains(d["agg_aff_source"], "uniform")
+        if contains(d[:agg_aff_source], "uniform")
             # use uniform remapped affinity map
             dend, dendValues = Process.forward(unfaff, sgm.seg)
         else
@@ -73,14 +86,14 @@ function aff2sgm(d::Dict{AbstractString, Any})
         end
         # create a new sgm, because Tsgm is immutable!
         @show dend
-        sgm = Tsgm(sgm.seg, dend, dendValues)
+        sgm = Tsgm(seg, dend, dendValues)
     end
     # save seg and mst
-    savesgm(d["fsgm"], sgm)
+    savesgm(d[:fsgm], sgm)
 end
 
-function aff2sgm(aff::Taff, d::Dict{ASCIIString, Any})
-    return aff2sgm(aff, d["low"], d["high"], d["thresholds"], d["dustsize"])
+function aff2sgm(aff::Taff, d::Dict{Symbol, Any})
+    return aff2sgm(aff, d[:low], d[:high], d[:thresholds], d[:dustsize])
 end
 
 function aff2sgm(aff::Taff, low::AbstractFloat=0.2, high::AbstractFloat=0.8, thresholds=[(1000,0.3)], dustsize=1000)
