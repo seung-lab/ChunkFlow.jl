024047b044dfd2f2809166105baeb51230417518
jingpengwu <jingpeng.wu@gmail.com>
2016-07-02 18:01:35 -0400

    add img2aff.jl
    Former-commit-id: 0e057e6501c4db1fe3664d23300e2f1b851618d0

---
+++ src/core/img2aff.jl
@@ -0,0 +1,138 @@
+export img2aff
+
+using EMIRT
+
+"""
+get image file name from an array or a filename
+"""
+function arr2path(tmpdir, inps::Dict, key::Symbol)
+    # automatic input transformation
+    if typeof( inps[key] ) <: Array
+        farr = joinpath(tmpdir, "$(key).h5")
+        imsave(inps[key], farr)
+        return farr
+    elseif typeof( inps[key] ) <: AbstractString
+        return inps[key]
+    end
+end
+
+"""
+create temporal dataset specification file
+"""
+function create_dataset_spec(tmpdir::AbstractString, inps::Dict)
+    fimg = arr2path(tmpdir, inps, :img)
+    dspec = """
+    [image1]
+    fnames = $(fimg)
+    pp_types = standard2D
+    is_auto_crop = yes
+
+    [sample1]
+    input = 1
+    """
+    # if has boundary map, it is the second stage
+    if haskey(inps, "bdr")
+        dspec = """
+        [image1]
+        fnames = $(fimg)
+        pp_types = standard2D
+        is_auto_crop = yes
+
+        [image10]
+        fnames = $(tmpdir)/out_sample1_output_0.tif
+        pp_types = symetric_rescale
+        is_auto_crop = yes
+        fmasks =
+
+        [sample1]
+        input = 1
+        rinput = 10
+        """
+    end
+
+    # write the spec file
+    f = open("$(tmpdir)/dataset.spec", "w")
+    write(f, dspec)
+    close(f)
+end
+
+function create_config(d)
+    tmpdir = d[:tmpdir]
+
+    # standard IO or not
+    is_stdio = d[:is_stdio] ? "yes" : "no"
+    is_bd_mirror = d[:is_bd_mirror] ? "yes" : "no"
+
+    outsz = d[:outsz]
+    # configuration string, note that spipe use x,y,z order,
+    # and ZNN use z,y,x order
+    conf="""
+    [parameters]
+    fnet_spec = $(d[:fnet_spec])
+    cost_fn = auto
+    fdata_spec = $(tmpdir)/dataset.spec
+    num_threads = 0
+    dtype = float32
+    out_type = $(d[:out_type])
+    forward_range = 1
+    is_bd_mirror = $(is_bd_mirror)
+    forward_net = $(d[:fnet])
+    is_stdio = $(is_stdio)
+    forward_conv_mode = $(d[:conv_mode])
+    forward_outsz = $(outsz[3]),$(outsz[2]),$(outsz[1])
+    output_prefix = $(tmpdir)/out
+    """
+    # write the config file
+    f = open("$(tmpdir)/forward.cfg", "w")
+    write(f, conf)
+    close(f)
+end
+
+function img2aff( d::Dict{Symbol, Any} )
+    if contains(d[:node_switch], "off")
+        return
+    end
+    println("znn forward pass...")
+    if isfile(d[:faff])
+        println("remove existing affinity file...")
+        rm(d[:faff])
+    end
+
+    inps = Dict( :img=> d[:fimg] )
+
+    tmpdir = d[:tmpdir]
+    # create dataset specification file
+    create_dataset_spec( tmpdir, inps )
+    # create forward pass stage 1 configuration file
+    create_config( d )
+    # current path
+    cp = pwd()
+    # run recursive forward pass
+    cd("$(d[:dir])/python")
+    run(`python forward.py -c $(tmpdir)/forward.cfg -n $(d[:fnet]) -r 1`)
+    cd(cp)
+    # move the output affinity to destination
+    outfname = joinpath(tmpdir, "out_sample1_output.h5")
+    fout = joinpath(tmpdir, "aff.h5")
+    ret = Dict( :aff=>fout )
+
+    # crop image, affinity map was already cropped in znn
+    # for ZNNi, we need to crop affinity map
+    if !d[:is_bd_mirror]
+        cropsize = (d[:fov]-1) / 2
+        # crop image
+        img = readimg(d[:fimg])
+        img = crop_border(img, cropsize)
+        rm(d[:fimg])
+        saveimg(d[:fimg], img, "main")
+        # crop affinity
+        # aff = readaff(outfname)
+        # rm(outfname)
+        # saveaff(outfname, aff, "main")
+    end
+    if outfname != d[:faff]
+        mv(outfname, fout, remove_destination=true)
+    end
+
+    return ret
+end
