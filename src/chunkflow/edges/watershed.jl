801f9530e0ba5b1deb6695c81f05c42d2991b6cd
jingpengwu <jingpeng.wu@gmail.com>
2016-07-11 16:44:03 -0400

    change config to JSON file, more structured code.
    Former-commit-id: 224df612d8a64d5d67ac9186f198c4bf190f9771

--- src/chunkflow/edges/watershed.jl
+++ src/chunkflow/edges/watershed.jl
@@ -1,54 +1,21 @@
-include("edge.jl")
-using EMIRT
 using Watershed
 using DataStructures
 
-export EdgeWatershed, forward!
+export ef_watershed!
 
-type EdgeWatershed <: AbstractEdge
-    kind::Symbol
-    params::Dict{Symbol, Any}
-    # the inputs and outputs are all nodes, which are in kvstore
-    inputs::Vector{Symbol}
-    outputs::Vector{Symbol}
-end
-
-function EdgeWatershed(conf::OrderedDict{UTF8String, Any})
-    kind = Symbol(conf["kind"])
-    @assert kind == :watershed
-    params = Dict{Symbol, Any}()
-    for (k,v) in conf["params"]
-        if k == "thresholds"
-            tmpl = Vector{Dict{Symbol,Any}}()
-            for d in v
-                tmpd = Dict{Symbol,Any}()
-                for (k2,v2) in d
-                    tmpd[Symbol(k2)] = v2
-                end
-                push!(tmpl, tmpd)
-            end
-            v = tmpl
-        end
-        params[Symbol(k)] = v
-    end
-    inputs = [Symbol(conf["inputs"][1])]
-    @assert length(conf["inputs"]) == 1
-    outputs = [Symbol(conf["outputs"][1])]
-    @assert length(conf["outputs"]) == 1
-
-    EdgeWatershed(kind, params, inputs, outputs)
-end
-
-function forward!( c::DictChannel, e::EdgeWatershed )
+"""
+edge function of watershed
+"""
+function ef_watershed!( c::DictChannel, e::Edge )
     println("-----------start watershed------------")
-    chk_aff = fetch(c, e.inputs[1])
+    chk_aff = fetch(c, e.inputs[:aff])
     aff = chk_aff.data
     @show size(aff)
     # check it is an affinity map
     @assert isa(aff, Taff)
 
     # use percentage threshold
-    b, count = hist(aff[:], 10000)
+    b, count = hist(aff[:], 100000)
     low  = percent2thd(b, count, e.params[:low])
     high = percent2thd(b, count, e.params[:high])
     thds = Vector{Tuple}()
@@ -60,11 +27,14 @@ function forward!( c::DictChannel, e::EdgeWatershed )
     # watershed
     println("watershed...")
     seg, rg = watershed(aff, low, high, thds, dust)
+    @show rg
+    @show typeof(rg)
     dend, dendValues = rg2dend(rg)
+    @show dend
     sgm = Tsgm( seg, dend, dendValues )
 
     # create chunk and put into channel
     chk_sgm = Chunk(sgm, chk_aff.origin, chk_aff.voxelsize)
-    put!(c, e.outputs[1], chk_sgm)
+    put!(c, e.outputs[:sgm], chk_sgm)
     println("-----------watershed end--------------")
 end
--- src/chunkflow/edges/znni.jl
+++ src/chunkflow/edges/znni.jl
@@ -1,36 +1,15 @@
-include("edge.jl")
 using EMIRT
 using HDF5
 using DataStructures
 
-export EdgeZNNi, forward!
+export ef_znni!
 
-type EdgeZNNi <: AbstractEdge
-    kind::Symbol
-    params::Dict{Symbol, Any}
-    # the inputs and outputs are all nodes, which are in kvstore
-    inputs::Vector{Symbol}
-    outputs::Vector{Symbol}
-end
-
-function EdgeZNNi(conf::OrderedDict{UTF8String, Any})
-    kind = Symbol(conf["kind"])
-    @assert kind == :znni
-    params = Dict{Symbol, Any}()
-    for (k,v) in conf["params"]
-        params[Symbol(k)] = v
-    end
-    inputs = [Symbol(conf["inputs"][1])]
-    @assert length(conf["inputs"]) == 1
-    outputs = [Symbol(conf["outputs"][1]), Symbol(conf["outputs"][2])]
-    @assert length(conf["outputs"]) == 2
-
-    EdgeZNNi(kind, params, inputs, outputs)
-end
-
-function forward!( c::DictChannel, e::EdgeZNNi)
+"""
+edge function of znni
+"""
+function ef_znni!( c::DictChannel, e::Edge)
     println("-----------start znni------------")
-    chk_img = fetch(c, e.inputs[1])
+    chk_img = fetch(c, e.inputs[:img])
     img = chk_img.data
     @assert isa(img, Timg)
 
@@ -48,8 +27,9 @@ function forward!( c::DictChannel, e::EdgeZNNi)
     # run znni inference
     currentdir = pwd()
     fznni = e.params[:fznni]
+    outsz = e.params[:outsz]
     cd(dirname(fznni))
-    run(`$(fznni) $(fimg) $(faff) main`)
+    run(`$(fznni) $(fimg) $(faff) main $(outsz[3]) $(outsz[2]) $(outsz[1])`)
     cd(currentdir)
 
     # read affinity map
@@ -57,10 +37,10 @@ function forward!( c::DictChannel, e::EdgeZNNi)
     chk_aff = Chunk(aff, chk_img.origin, chk_img.voxelsize)
     # crop img and aff
     cropsize = (e.params[:fov]-1)./2
-    chk_img = crop_border(chk_img, cropsize)
-    chk_aff = crop_border(chk_aff, cropsize)
+    chk_img = crop_border!(chk_img, cropsize)
+    chk_aff = crop_border!(chk_aff, cropsize)
 
-    put!(c, e.outputs[1], chk_img)
-    put!(c, e.outputs[2], chk_aff)
+    put!(c, e.outputs[:img], chk_img)
+    put!(c, e.outputs[:aff], chk_aff)
     println("-------znni end-------")
 end
