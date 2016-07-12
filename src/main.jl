d451f4676ebcfe665b683fd4485ad9abc72e1374
jingpengwu <jingpeng.wu@gmail.com>
2016-07-12 18:51:28 -0400

    works with aws sqs, distributed version!
    Former-commit-id: 0992a97a4e2017c5e1bc35ccee498844eb564b31

--- src/main.jl
+++ src/main.jl
@@ -1,71 +1,9 @@
-using EMIRT
+include("aws/task.jl")
+include("network/network.jl")
 
-include("core/aff2sgm.jl")
-include("core/sgm2omprj.jl")
-include("core/img2aff.jl")
-include("core/aws.jl")
-include("core/task.jl")
-include(joinpath(Pkg.dir(), "EMIRT/src/plugins/aws.jl"))
+task = get_task()
 
-const global env = build_env()
-const global queuename = "spipe-tasks"
+@show task
 
-function main()
-    if length(ARGS)>0
-        # the task information was embedded in a dictionary
-        pd = get_task(env)
-        # do this task
-        handletask(pd)
-    else
-        # keep fetching tasks from AWS SQS, and keep busy doing tasks
-        while true
-            is_auto_shutdown = false
-            try
-                # the task information was embedded in a dictionary
-                pd = get_task()
-                # update the status of auto shutdown
-                is_auto_shutdown = pd[:gn][:is_auto_shutdown]
-                # do this task
-                handletask(pd)
-                # avoid endless loop for local use
-                if length(ARGS) > 0
-                    if is_auto_shutdown
-                        run(`sudo shutdown -h 0`)
-                    else
-                        break
-                    end
-                end
-            catch
-                # auto shutdown
-                if is_auto_shutdown
-                    run(`sudo shutdown -h 0`)
-                else
-                    backtrace()
-                    error("no task to do!")
-                end
-            end
-        end
-    end
-end
-
-"""
-handle a task
-"""
-function handletask( pd::Tpd )
-    println("start doing a task...")
-    # znn forward pass to get affinity map
-    # file name to save affinity map
-    img2aff( pd[:znn] )
-
-    # watershed and aggromeration
-    aff2sgm(pd[:ws])
-
-    # omnification
-    sgm2omprj(pd[:omni])
-
-    # move results
-    mvoutput(pd[:omni])
-end
-
-# run the main function
-main()
+net = Net(task)
+forward(net)
--- src/network/edges/readh5.jl
+++ src/network/edges/readh5.jl
@@ -1,11 +1,38 @@
 using HDF5
 
 include(joinpath(Pkg.dir(), "EMIRT/src/plugins/aws.jl"))
-include("../chunk.jl")
+include("../../chunk/chunk.jl")
 
 using DataStructures
 
 export ef_readh5!
+
+"""
+extract offset from file name
+"""
+function fname2offset(fname::AbstractString)
+    # initialize the offset
+    offset = Vector{Int64}()
+
+    bn = basename(fname)
+    name, ext = splitext(bn)
+    # substring list
+    strlst = split(name, "_")
+    for str in strlst
+        if contains(str, "-")
+            strlst2 = split(str, "-")
+            if typeof(parse(strlst2[1]))<:Int64
+                push!(offset, parse(strlst2[1]))
+            end
+        end
+    end
+    if length(offset)==3
+        return offset
+    else
+        warn("invalid auto offset, use default [0,0,0]!")
+        return Vector{UInt32}([0,0,0])
+    end
+end
 
 """
 edge function of readh5
@@ -28,10 +55,12 @@ function ef_readh5!(c::DictChannel,
         origin[1] = h5read(fname, "x_slice")[1]
         origin[2] = h5read(fname, "y_slice")[1]
         origin[3] = h5read(fname, "z_slice")[1]
+    else
+        origin = fname2offset(fname)
     end
     close(f)
     voxelsize = params[:voxelsize]
     chk = Chunk(arr, origin, voxelsize)
     # put chunk to channel for use
-    put!(c, outputs[:img], chk)
+    put!(c, outputs[:data], chk)
 end
