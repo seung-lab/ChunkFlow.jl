ecbbcf3013c6c070eba706804b5335b49eeb24ec
jingpengwu <jingpeng.wu@gmail.com>
2016-07-18 18:05:07 -0400

    produce chunks and tasks together
    Former-commit-id: e203beaab7d4668538150ce30d92cae2367cb316

--- src/aws/task.jl
+++ src/aws/task.jl
@@ -1,9 +1,12 @@
 using JSON
 using DataStructures
 
+typealias Ttask OrderedDict{Symbol, Any}
+
 include(joinpath(Pkg.dir(), "EMIRT/src/plugins/aws.jl"))
 
-const sqsname = "spipe-tasks"
+global const sqsname = "spipe-tasks"
+global const env = build_env()
 
 export get_task
 
@@ -19,16 +22,16 @@ function get_s3_task(fname::AbstractString)
     @assert iss3(fname)
     env = build_env()
     lcfile = download(env, ARGS[1], "/tmp/")
-    task = readlines( lcfile )
+    str_task = readall( lcfile )
     # transform text to JSON OrderedDict format
-    return JSON.parse(task, dicttype=OrderedDict{Symbol, Any})
+    return JSON.parse(str_task, dicttype=OrderedDict{Symbol, Any})
 end
 
 function get_local_task(fname::AbstractString)
     # just simple local file
-    task = readall( fname )
+    str_task = readall( fname )
     # transform text to JSON OrderedDict format
-    return JSON.parse(task, dicttype=OrderedDict{Symbol, Any})
+    return JSON.parse(str_task, dicttype=OrderedDict{Symbol, Any})
 end
 
 """
@@ -37,17 +40,30 @@ get task from AWS Simple queue
 function get_task()
     # parse the config file
     if length(ARGS)==0
-        return get_sqs_task()
+        task = get_sqs_task()
     else
         if length(ARGS)>1
             warn("too many input arguments, use the first one only!")
         end
         if iss3( ARGS[1] )
-            return get_s3_task(ARGS[1])
+            task = get_s3_task(ARGS[1])
         elseif isfile(ARGS[1])
-            return get_local_task(ARGS[1])
+            task = get_local_task(ARGS[1])
         else
             error("input should be a s3 or local task configuration file in JSON format!")
         end
     end
+    return task
+end
+
+"""
+transfer dict task to string
+"""
+function task2str(task::Ttask)
+    # convert to string
+    ftmp = tempname()
+    f = open(ftmp, "w")
+    JSON.print(f, task)
+    close(f)
+    str_task = readall(ftmp)
 end
