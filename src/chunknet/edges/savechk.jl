b521202e8086365e4c5484e20ae32dc969efa04c
jingpengwu <jingpeng.wu@gmail.com>
2016-08-18 10:55:07 -0400

    add some missing files
    Former-commit-id: ce8f48afe53200981fe652bdf09448188fb50548
    Former-commit-id: f74ac76064b2a559aafd0fa181ff54e4adb4b86e

---
+++ src/chunknet/edges/savechk.jl
@@ -0,0 +1,37 @@
+# save chunk from dictchannel to local disk or aws s3
+
+include(joinpath(Pkg.dir(), "EMIRT/plugins/aws.jl"))
+include("../../chunk/chunk.jl")
+
+using DataStructures
+
+"""
+edge function of readh5
+"""
+function ef_savechk(c::DictChannel,
+                    params::OrderedDict{Symbol, Any},
+                    inputs::OrderedDict{Symbol, Any},
+                    outputs::OrderedDict{Symbol, Any} )
+    # get chunk
+    chk = take!(c, inputs[:chunk])
+    origin = chk.origin
+    voxelsize = chk
+    if haskey(outputs, :fname)
+        fname = outputs[:fname]
+        @assert contains(fname, ".h5")
+    else
+        prefix = replace(outputs[:prefix],"~",homedir())
+        fname = "$(prefix)$(chk.origin[1])_$(chk.origin[2])_$(chk.origin[3]).$(inputs[:chunk]).h5"
+    end
+    if iss3(fname)
+        ftmp = "/tmp/chk.h5"
+        save(ftmp, chk)
+        run(`aws s3 mv $ftmp $fname`)
+    else
+        save(fname, chk)
+    end
+    # put the input chunk back to channel
+    if !params[:isdelete]
+        put!(c, inputs[:chunk], chk)
+    end
+end
---
+++ src/chunknet/types.jl
@@ -0,0 +1,1 @@
+typealias OrderedDict{Symbol, Any} Ttask
