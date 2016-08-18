ebe7edb7e0398d875f8efb26a0b9996d348bbe6d
jingpengwu <jingpeng.wu@gmail.com>
2016-08-18 10:55:07 -0400

    add some missing files
    Former-commit-id: eccc54ea9210ce286d3f3bc52b14322d191ba2b9

---
+++ src/chunknet/edges/readchk.jl
@@ -0,0 +1,29 @@
+using HDF5
+
+include(joinpath(Pkg.dir(), "EMIRT/plugins/aws.jl"))
+include("../../chunk/chunk.jl")
+
+using DataStructures
+
+"""
+edge function of readh5
+"""
+function ef_readchk!(c::DictChannel,
+                    params::OrderedDict{Symbol, Any},
+                    inputs::OrderedDict{Symbol, Any},
+                    outputs::OrderedDict{Symbol, Any} )
+    fname = inputs[:fname]
+    if iss3(fname)
+        # download from s3
+        env = build_env()
+        fname = download(env, fname, "/tmp/")
+    end
+    @show fname
+    chk = readchk(fname)
+    # put chunk to channel for use
+    put!(c, outputs[:data], chk)
+
+    # release memory
+    chk = nothing
+    gc()
+end
