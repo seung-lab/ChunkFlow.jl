065c3216cc5bd449dc5a5573e1c732f7dacf381d
jingpengwu <jingpeng.wu@gmail.com>
2016-08-31 17:17:01 -0400

    add arg parser; add option to set gpu id; change Ttask to ChunkFlowTask
    Former-commit-id: e70e34e8757fecaefe4b75f3cb34d5819d374e81

--- src/main.jl
+++ src/main.jl
@@ -1,4 +1,5 @@
 include("aws/task.jl")
+include("core/argparser.jl")
 include("chunknet/ChunkNet.jl")
 
 using Logging
@@ -7,20 +8,33 @@ Logging.configure(filename="logfile.log")
 
 using ChunkNet
 
-if length(ARGS) >0
-    task = get_task(ARGS[1])
+# parse the arguments as a dictionary, key is string
+argDict = parse_commandline()
+@show argDict
+
+if haskey(argDict, "task")
+  # has local task definition
+  task = get_task(argDict["task"])
+  if haskey(argDict, "gpuid")
+    set_gpu_id!(task, argDict["gpuid"])
+  end
+  net = Net(task)
+  forward(net)
+else
+  # fetch task from AWS SQS
+  while true
+    task, msg = get_task()
+
+    # set the gpu device id to use
+    if haskey(argDict, "gpuid")
+      set_gpu_id!(task, argDict["gpuid"])
+    end
+    @show task
+
     net = Net(task)
     forward(net)
-else
-    while true
-        task, msg = get_task()
-
-        @show task
-
-        net = Net(task)
-        forward(net)
-        # delete task message in SQS
-        deleteSQSmessage!(env, msg, sqsname)
-        sleep(5)
-    end
+    # delete task message in SQS
+    deleteSQSmessage!(env, msg, sqsname)
+    sleep(5)
+  end
 end
