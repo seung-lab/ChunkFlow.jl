22e26a4a36909a7055d0390a6fe6aad9e0b6f97f
jingpengwu <jingpeng.wu@gmail.com>
2016-08-31 13:48:24 -0400

    tested, working version!
    Former-commit-id: 74edce698cf758ce8273f452fcab54b10a3f7676
    Former-commit-id: 1c8029a12ca882c745ebf19e2392a2b3827a13fe

--- src/chunk/chunk.jl
+++ src/chunk/chunk.jl
@@ -6,57 +6,68 @@ abstract AbstractChunk
 #include(joinpath(Pkg.dir(), "EMIRT/plugins/aws.jl"))
 
 type Chunk <: AbstractChunk
-    data::Union{Array, Tsgm}                  # could be 3-5 Dimension dataay
+    data::Union{Array, SegMST}                  # could be 3-5 Dimension dataay
     origin::Vector     # measured by voxel number
     voxelsize::Vector  # physical size of each voxel
 end
 
-function crop_border!{T}( chk::Chunk, cropMarginSize::Union{Vector{T},Tuple{T}} )
-    @assert typeof(chk.data) <: Array
-    nd = ndims(chk.data)
-    @assert nd >= 3
-    sz = size(chk.data)
-    @assert sz[1]>cropMarginSize[1]*2 &&
-            sz[2]>cropMarginSize[2]*2 &&
-            sz[3]>cropMarginSize[3]*2
-    if nd == 3
-        chk.data = chk.data[cropMarginSize[1]+1:sz[1]-cropMarginSize[1],
-                            cropMarginSize[2]+1:sz[2]-cropMarginSize[2],
-                            cropMarginSize[3]+1:sz[3]-cropMarginSize[3]]
-    elseif nd==4
-        chk.data = chk.data[cropMarginSize[1]+1:sz[1]-cropMarginSize[1],
-                            cropMarginSize[2]+1:sz[2]-cropMarginSize[2],
-                            cropMarginSize[3]+1:sz[3]-cropMarginSize[3], :]
-    elseif nd==5
-        chk.data = chk.data[cropMarginSize[1]+1:sz[1]-cropMarginSize[1],
-                            cropMarginSize[2]+1:sz[2]-cropMarginSize[2],
-                            cropMarginSize[3]+1:sz[3]-cropMarginSize[3], :, :]
-    else
-        error("only support 3-5 D, current dataay dimention is $(nd)")
-    end
-    chk.origin += cropMarginSize
-    chk
+"""
+crop the 3D surrounding margin
+"""
+function crop_border{T}(chk::Chunk, cropMarginSize::Union{Vector{T},Tuple{T}})
+  @assert typeof(chk.data) <: Array
+  nd = ndims(chk.data)
+  @assert nd >= 3
+  sz = size(chk.data)
+  @assert sz[1]>cropMarginSize[1]*2 &&
+          sz[2]>cropMarginSize[2]*2 &&
+          sz[3]>cropMarginSize[3]*2
+  if nd == 3
+      data = chk.data[cropMarginSize[1]+1:sz[1]-cropMarginSize[1],
+                      cropMarginSize[2]+1:sz[2]-cropMarginSize[2],
+                      cropMarginSize[3]+1:sz[3]-cropMarginSize[3]]
+  elseif nd==4
+      data = chk.data[cropMarginSize[1]+1:sz[1]-cropMarginSize[1],
+                      cropMarginSize[2]+1:sz[2]-cropMarginSize[2],
+                      cropMarginSize[3]+1:sz[3]-cropMarginSize[3], :]
+  elseif nd==5
+      data = chk.data[cropMarginSize[1]+1:sz[1]-cropMarginSize[1],
+                      cropMarginSize[2]+1:sz[2]-cropMarginSize[2],
+                      cropMarginSize[3]+1:sz[3]-cropMarginSize[3], :, :]
+  else
+      error("only support 3-5 D, current dataay dimention is $(nd)")
+  end
+  origin = chk.origin .+ cropMarginSize
+  Chunk(data, origin, chk.voxelsize)
 end
 
+"""
+compute the physical offset
+"""
 function physical_offset( chk::Chunk )
     Vector{UInt32}((chk.origin-1) .* chk.voxelsize)
 end
 
+"""
+save chunk in a hdf5 file
+"""
 function save(fname::AbstractString, chk::Chunk)
     if isfile(fname)
         rm(fname)
     end
     f = h5open(fname, "w")
     f["type"] = "chunk"
-    if isa(chk.data, Taff)
-        # save with compression
-        f["affinityMap", "chunk", (64,64,8,3), "shuffle", (), "deflate", 3] = chk.data
-    elseif isa(chk.data, Timg) || isa(chk.data, Tseg)
-        f["image", "chunk", (64,64,8), "shuffle", (), "deflate", 3] = chk.data
-    elseif isa(chk.data, Tsgm)
-        f["segmentation", "chunk", (64,64,8), "shuffle", (), "deflate", 3] = chk.data.seg
+    if isa(chk.data, AffinityMap)
+      # save with compression
+      f["affinityMap", "chunk", (64,64,8,3), "shuffle", (), "deflate", 3] = chk.data
+    elseif isa(chk.data, EMImage)
+      f["image", "chunk", (64,64,8), "shuffle", (), "deflate", 3] = chk.data
+    elseif isa(chk.data, Segmentation)
+      f["segmentation", "chunk", (64,64,8), "shuffle", (), "deflate", 3] = chk.data
+    elseif isa(chk.data, SegMST)
+        f["segmentation", "chunk", (64,64,8), "shuffle", (), "deflate", 3] = chk.data.segmentation
         f["segmentPairs"] = chk.data.segmentPairs
-        f["segmentAffinity"] = chk.data.segmentPairWeights
+        f["segmentPairAffinities"] = chk.data.segmentPairAffinities
     else
         error("This is an unsupported type: $(typeof(chk.data))")
     end
