7928f2a161b9b71438eed2a0cf9c1b8b08f4f415
jingpengwu <jingpeng.wu@gmail.com>
2016-08-31 20:52:29 -0400

    assign id of gpu
    Former-commit-id: 809db7e7c809081ebb4aead53122f7612c2026a4
    Former-commit-id: c43880cf8789249411fa54b234f93a9ca1d0beaa

--- src/main.jl
+++ src/main.jl
@@ -1,21 +1,19 @@
 include("aws/task.jl")
 include("core/argparser.jl")
 include("chunknet/ChunkNet.jl")
-
+using ChunkNet
 using Logging
 @Logging.configure(level=DEBUG)
 Logging.configure(filename="logfile.log")
-
-using ChunkNet
 
 # parse the arguments as a dictionary, key is string
 argDict = parse_commandline()
 @show argDict
 
-if haskey(argDict, "task")
+if !isa(argDict["task"], Void)
   # has local task definition
   task = get_task(argDict["task"])
-  if haskey(argDict, "gpuid")
+  if !isa(argDict["gpuid"], Void)
     set_gpu_id!(task, argDict["gpuid"])
   end
   net = Net(task)
@@ -26,7 +24,7 @@ else
     task, msg = get_task()
 
     # set the gpu device id to use
-    if haskey(argDict, "gpuid")
+    if !isa(argDict["gpuid"], Void)
       set_gpu_id!(task, argDict["gpuid"])
     end
     @show task
