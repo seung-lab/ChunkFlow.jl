09b8dab15d8d6483607187b58a80f543e50fc0f4
jingpengwu <jingpeng.wu@gmail.com>
2016-09-01 21:26:47 -0400

    add upload support
    Former-commit-id: 7c389ebb57445bb5e75e8e80c3a6fbf26f39a8b5
    Former-commit-id: 0be74d3880f1f16c47c1fe6a2032f0e07d207bee

--- src/chunknet/edges/movedata.jl
+++ src/chunknet/edges/movedata.jl
@@ -9,14 +9,29 @@ function ef_movedata(c::DictChannel,
                     params::OrderedDict{Symbol, Any},
                     inputs::OrderedDict{Symbol, Any},
                     outputs::OrderedDict{Symbol, Any} )
-    # get chunk
-    fin = replace(inputs[:prefix], "~", homedir())
-    fot = replace(outputs[:dir], "~", homedir())
+  # get chunk
+  fin = replace(inputs[:prefix], "~", homedir())
+  fot = replace(outputs[:dir], "~", homedir())
+  if iss3(fin)
+    download(awsEnv, fin, fot)
+  elseif iss3(fot)
+    upload(awsEnv, fin, fot)
+    if params[:isRemoveSourceFile]
+      rm(fin)
+    end
+  else
+    # local movement of files
     for fbase in readdir(dirname(fin))
         if contains(fbase, basename(fin))
             fout = joinpath(fot, fbase)
-            mv(joinpath(dirname(fin), fbase),
-                joinpath(dirname(fot), fbase), remove_destination=true)
+            if params[:isRemoveSourceFile]
+              mv(joinpath(dirname(fin), fbase),
+                  joinpath(dirname(fot), fbase), remove_destination=true)
+            else
+              cp(joinpath(dirname(fin), fbase),
+                  joinpath(dirname(fot), fbase), remove_destination=true)
+            end
         end
     end
+  end
 end
