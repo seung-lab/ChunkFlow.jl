8bd7de3fbca4075c2dd764402ec5d94fb503f239
jingpengwu <jingpeng.wu@gmail.com>
2016-09-05 15:51:56 -0400

    add mergeseg edge
    Former-commit-id: 20753f7f33acf9a046406b7dc7bc664114b7dd7f

---
+++ src/edges/mergeseg.jl
@@ -0,0 +1,17 @@
+using EMIRT
+
+"""
+edge function of watershed
+"""
+function ef_mergeseg!( c::DictChannel,
+                params::OrderedDict{Symbol, Any},
+                inputs::OrderedDict{Symbol, Any},
+                outputs::OrderedDict{Symbol, Any})
+    chk_sgm = fetch(c, inputs[:sgm])
+
+    # watershed
+    println("merge segmentation...")
+    merge!(chk_sgm.data, params[:threshold])
+
+    put!(c, outputs[:sgm], chk_sgm)
+end
