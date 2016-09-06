90c922b0435df2e7cf78ac5b043434de63cbd355
jingpengwu <jingpeng.wu@gmail.com>
2016-09-06 16:59:10 -0400

    no need to pass awsEnv;
    Former-commit-id: d98ed4fb1a2d911642078fd4d7770933b77937ff

--- src/aws/task.jl
+++ src/aws/task.jl
@@ -2,7 +2,7 @@ using JSON
 using DataStructures
 
 include("../core/types.jl")
-include(joinpath(Pkg.dir(), "EMIRT/plugins/aws.jl"))
+include(joinpath(Pkg.dir(), "EMIRT/plugins/cloud.jl"))
 
 """
 set the gpuid for all functions
@@ -16,7 +16,7 @@ function set_gpu_id!(task::ChunkFlowTask, gpuid::Int)
 end
 
 function get_sqs_task(queuename::AbstractString = sqsname)
-    msg = fetchSQSmessage(awsEnv, ASCIIString(queuename))
+    msg = fetchSQSmessage(ASCIIString(queuename))
     task = msg.body
     # transform text to JSON OrderedDict format
     return JSON.parse(task, dicttype=OrderedDict{Symbol, Any}), msg
@@ -24,7 +24,7 @@ end
 
 function get_s3_task(fname::AbstractString)
     @assert iss3(fname)
-    lcfile = download(awsEnv, ARGS[1], "/tmp/")
+    lcfile = download(ARGS[1], "/tmp/")
     str_task = readall( lcfile )
     # transform text to JSON OrderedDict format
     return JSON.parse(str_task, dicttype=OrderedDict{Symbol, Any})
@@ -45,12 +45,12 @@ function get_task()
 end
 
 function get_task(ftask::AbstractString)
-    if contains( ftask, "s3://" )
+    if iss3( ftask )
         task = get_s3_task( ftask )
     elseif isfile( ftask )
         task = get_local_task( ftask )
     else
-        error("input should be a s3 or local task configuration file in JSON format!")
+        error("input should be a s3 or local task configuration file in JSON format! \n input task file: $ftask")
     end
 end
 
@@ -65,7 +65,7 @@ function produce_tasks_s3img(task::ChunkFlowTask)
     for key in keylst
         task[:input][:inputs][:fname] = joinpath("s3://", bkt, key)
         # send the task to SQS queue
-        sendSQSmessage(awsEnv, sqsname, JSON.json(task))
+        sendSQSmessage(sqsname, JSON.json(task))
     end
 end
 
@@ -85,6 +85,6 @@ function produce_tasks_local(task::ChunkFlowTask)
         end
         task[:input][:inputs][:fname] = joinpath(dn, fname)
         str_task = task2str(task)
-        sendSQSmessage(awsEnv, sqsname, str_task)
+        sendSQSmessage(sqsname, str_task)
     end
 end
