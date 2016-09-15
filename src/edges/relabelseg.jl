65cfddfd4b8ac1b24d291ade98609e7a7b441751
jingpengwu <jingpeng.wu@gmail.com>
2016-09-14 22:57:07 -0400

    add edge to cutout from bigarray; add edge to relabel segmentation; replace voxelsize with voxelSize
    Former-commit-id: b844a5fc69460a11d9f14e1fd3176aa77cfad981
    Former-commit-id: bc37c7b93e0e93e79f4c8ee29d6ec2a51ec62d9e

---
+++ src/edges/relabelseg.jl
@@ -0,0 +1,17 @@
+using EMIRT
+
+"""
+edge function of watershed
+"""
+function ef_relabelseg!( c::DictChannel,
+                params::OrderedDict{Symbol, Any},
+                inputs::OrderedDict{Symbol, Any},
+                outputs::OrderedDict{Symbol, Any})
+    chk_sgm = take!(c, inputs[:sgm])
+
+    # watershed
+    println("merge segmentation...")
+    chk_sgm.data = relabel_seg( chk_sgm.data )
+
+    put!(c, outputs[:sgm], chk_sgm)
+end
