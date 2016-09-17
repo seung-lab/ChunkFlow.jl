d09d4eba06b9d3d1297f18f37147a0607faa190a
jingpengwu <jingpeng.wu@gmail.com>
2016-09-17 16:29:27 -0400

    run 2 stage processing
    Former-commit-id: e647819a3dac553df297d61d113ade031fc3290d

--- src/edges/crop.jl
+++ src/edges/crop.jl
@@ -5,7 +5,6 @@ function ef_crop!( c::DictChannel,
                 params::OrderedDict{Symbol, Any},
                 inputs::OrderedDict{Symbol, Any},
                 outputs::OrderedDict{Symbol, Any})
-    info("-----------crop-------------")
     for (k,v) in inputs
         @assert haskey(outputs, k)
         chk = take!(c, v)
