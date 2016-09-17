8b69eaf2f28e6b53799e6545c14b6b976f42b843
jingpengwu <jingpeng.wu@gmail.com>
2016-09-17 16:29:27 -0400

    run 2 stage processing
    Former-commit-id: 5eccd0b2a8c365de9d0cba108ed7237535404afa
    Former-commit-id: 3c3dc23becb444342f586cff7d2007a10de0b8ac

--- src/edges/cutoutchunk.jl
+++ src/edges/cutoutchunk.jl
@@ -16,29 +16,29 @@ function ef_cutoutchunk!(c::DictChannel,
     elseif  contains( params[:bigArrayType], "H5" ) ||
             contains(params[:bigArrayType], "h5") ||
             contains( params[:bigArrayType], "hdf5" )
-      ba = H5sBigArray( inputs[:h5sDir];
-                        blockSize = params[:blockSize],
-                        chunkSize = params[:chunkSize] )
+      ba = H5sBigArray( inputs[:h5sDir] )
     else
       error("invalid bigarray type: $(params[:bigArrayType])")
     end
 
     # get range
     origin = params[:origin]
-    size = params[:blockSize]
+    chunkSize = params[:chunkSize]
     # cutout as chunk
     if ndims(ba)==3
-      data = ba[origin[1] : origin[1]+size[1]-1,
-                origin[2] : origin[2]+size[2]-1,
-                origin[3] : origin[3]+size[3]-1]
+      data = ba[origin[1] : origin[1]+chunkSize[1]-1,
+                origin[2] : origin[2]+chunkSize[2]-1,
+                origin[3] : origin[3]+chunkSize[3]-1]
     else
       @assert ndims(ba)==4
-      data = ba[origin[1] : origin[1]+size[1]-1,
-                origin[2] : origin[2]+size[2]-1,
-                origin[3] : origin[3]+size[3]-1, :]
+      data = ba[origin[1] : origin[1]+chunkSize[1]-1,
+                origin[2] : origin[2]+chunkSize[2]-1,
+                origin[3] : origin[3]+chunkSize[3]-1, :]
     end
 
     chk = Chunk(data, origin, params[:voxelSize])
+
+    println("cout out chunk size: $(size(data))")
 
     # put chunk to channel for use
     put!(c, outputs[:data], chk)
