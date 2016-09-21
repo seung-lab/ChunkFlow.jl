b0f39b267ac9a2e6243e0b1c6539fc252ac10cec
jingpengwu <jingpeng.wu@gmail.com>
2016-09-20 22:59:35 -0400

    add edge to remove data from dictchannel, could be added for auto-optimization; also crop the affinity map before agglomeration.
    Former-commit-id: 42615bd994fdbb4e6a380adbe56710363568d897
    Former-commit-id: e5bcdf035d5bb2898eff3fc7478baa44055d3e04

--- src/edges/agglomeration.jl
+++ src/edges/agglomeration.jl
@@ -19,6 +19,7 @@ function ef_agglomeration!( c::DictChannel,
     # and segmentation with mst
     @assert isa(chk_seg.data, Segmentation)
     @assert isa(chk_aff.data, AffinityMap)
+    # @assert size(chk_aff)[1:3] == size(chk_seg)
 
     # run watershed
     segmentPairs, segmentPairAffinities = Process.forward(chk_aff.data, chk_seg.data)
