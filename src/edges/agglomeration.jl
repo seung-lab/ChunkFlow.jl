f303f91b097a8e116141a325f205486f823a03cb
jingpengwu <jingpeng.wu@gmail.com>
2016-11-08 17:44:59 -0500

    put ChunkFlow as a julia package; add mask affinity border to do agglomeration;
    Former-commit-id: 1dc869103c7772aa0e88c33c2a6516773409bca9
    Former-commit-id: 280decbee9f5fc7b03619523ae6578e4e9b5c7b4

--- src/edges/agglomeration.jl
+++ src/edges/agglomeration.jl
@@ -21,9 +21,28 @@ function ef_agglomeration!( c::DictChannel,
     @assert isa(chk_aff.data, AffinityMap)
     # @assert size(chk_aff)[1:3] == size(chk_seg)
 
-    # run watershed
-    segmentPairs, segmentPairAffinities = Process.forward(chk_aff.data, chk_seg.data)
-    sgm = SegMST(chk_seg.data, segmentPairs, segmentPairAffinities)
+    if haskey(params, :maskAffinityMarginSize)
+        maskAffinityMarginSize = params[:maskAffinityMarginSize]
+        chk_aff.data[   1:maskAffinityMarginSize[1],
+                        1:maskAffinityMarginSize[2],
+                        1:maskAffinityMarginSize[3], :] = 0f0
+        chk_aff.data[   end-maskAffinityMarginSize[1]:end,
+                        end-maskAffinityMarginSize[2]:end,
+                        end- maskAffinityMarginSize[3]:end, : ] = 0f0
+    end
+
+    if haskey(params, :cropSegMarginSize)
+        chk_seg_out = BigArrays.crop_border(chk_seg, params[:cropSegMarginSize])
+        segids = Set{eltype(chk_seg_out.data)}()
+        for i in eachindex(chk_seg_out.data)
+           push!(segids, chk_seg_out.data[i])
+        end
+        segmentPairs, segmentPairAffinities = Process.forward(chk_aff.data, chk_seg.data, segids)
+        sgm = SegMST(chk_seg_out.data, segmentPairs, segmentPairAffinities)
+    else
+        segmentPairs, segmentPairAffinities = Process.forward(chk_aff.data, chk_seg.data)
+        sgm = SegMST(chk_seg.data, segmentPairs, segmentPairAffinities)
+    end
     chk_sgm = Chunk(sgm, chk_aff.origin[1:3], chk_aff.voxelSize)
 
     # put output to channel
