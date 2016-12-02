66d16c9ab69894ac7ab1c513819e9a79ad7f69d0
jingpengwu <jingpeng.wu@gmail.com>
2016-12-02 15:39:03 -0500

    multiple processes processing
    Former-commit-id: 5a778cabbe35d410f874a16fd007640d2ede19b8

--- scripts/main.jl
+++ scripts/main.jl
@@ -1,13 +1,25 @@
 # include("../src/core/argparser.jl")
-include("../src/ChunkNet.jl")
-using ChunkNet
-using ChunkNet.Execute
-using Logging
+# include("../src/ChunkFlow.jl")
+using Agglomeration, Process
+@everywhere using Agglomeration, Process
+@everywhere using ChunkFlow
+@everywhere using ChunkFlow.Execute
+@everywhere using Logging
 @Logging.configure(level=DEBUG)
 Logging.configure(filename="logfile.log")
 
 # parse the arguments as a dictionary, key is string
-argDict = parse_commandline()
+@everywhere argDict = parse_commandline()
 @show argDict
 
-execute( argDict )
+@sync begin
+    for p in 1:nworkers()
+        @async begin
+            remotecall_wait(execute, p, argDict)
+        end
+    end
+end
+
+# @parallel for i in 1:nworkers()
+#     execute( argDict )
+# end
