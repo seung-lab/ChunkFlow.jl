acb3443855f305d7242e51d08c75ccaeabfae0eb
jingpengwu <jingpeng.wu@gmail.com>
2017-03-10 22:14:01 -0500

    work with merge segmentation
    Former-commit-id: 6ce349650a7b0231338da813760417dcf3fd0ddd
    Former-commit-id: 42e590906422b45c06ed7bd76481bb0e8ea4707c

--- src/utils/producer.jl
+++ src/utils/producer.jl
@@ -2,29 +2,75 @@ module Producer
 
 using ..ChunkFlow
 using DataStructures
+using BigArrays.Utils
 
-export submit_chunk_task, taskproducer
+export submit_chunk_task, taskproducer, get_origin_set
 
-function get_origin_set()
-    fileNames = readstring(`gsutil ls gs://zfish/all_7/hypersquare/`)
-    fileList = split(fileNames)
+function get_origin_set( fileNameList::Vector )
+    # fileNames = readstring(`gsutil ls gs://zfish/all_7/hypersquare/`)
+    # fileList = split(fileNames)
     originSet = Set()
-    for fileName in fileList
-        fileName = split(fileName,"/")[end-1]
+    for fileName in fileNameList
+        # fileName = split(fileName,"/")[end-1]
         #     @show fileName
-        fields = split(fileName, "_")[2:end]
-        origin = map(x->parse(split(x,"-")[1]), fields)
+        # fields = split(fileName, "_")[2:end]
+        # origin = map(x->parse(split(x,"-")[1]), fields)
         #     @show origin
-        origin .-= [64,64,8]
-        push!(origin, 1)
+        # origin .-= [64,64,8]
+        # push!(origin, 1)
+        origin = fileName2origin( fileName; prefix = "block_" )
         push!(originSet, origin)
+    end
+    return originSet
+end
+
+function get_origin_set(argDict::Dict)
+    # cut out from a big array
+    N = length(argDict[:gridsize])
+    gridIndexList = Vector{Tuple}()
+    originSet = OrderedSet{Vector}()
+    # the flag to indicate whether the specific origin was visited
+    flag = false
+    for gridz in 1:argDict[:gridsize][3]
+        for gridy in 1:argDict[:gridsize][2]
+            for gridx in 1:argDict[:gridsize][1]
+                if 3 < N
+                    # push!(gridIndexList,
+                    #         (gridx, gridy, gridz,
+                    #             ones(Int, N - 3)...))
+                    gridIndex = (gridx, gridy, gridz,
+                                    ones(Int, N - 3)...)
+                else
+                    # push!(gridIndexList, (gridx, gridy, gridz))
+                    gridIndex = (gridx, gridy, gridz)
+                end
+                origin = argDict[:origin] .+ ([gridIndex...] .- 1) .* argDict[:stride]
+                #if flag
+                    push!(originSet, origin)
+                #end
+                if origin == [47105,6145,257]
+                   flag = true
+                end
+            end
+        end
     end
     return originSet
 end
 
 # const originSet = get_origin_set()
 # println("originSet = $(originSet)")
+"""
+    submit_chunk_task(argDict::Dict{Symbol, Any}, origin::Vector, task::ChunkFlowTask)
+submit a task corresponds to a specific chunk
 
+Parameters:
+- argDict: commandline arguments
+- origin:  the origin of this specific chunk
+- task: the Dict defines computation task and parameters
+
+Output:
+- generate a task dict and turns to string, submit it to SQS queue
+"""
 function submit_chunk_task(argDict::Dict{Symbol, Any},
                             origin::Vector,
                             task::ChunkFlowTask)
@@ -84,7 +130,7 @@ function submit_chunk_task(argDict::Dict{Symbol, Any},
     submit(task; sqsQueueName = argDict[:awssqs])
 end
 
-function taskproducer( argDict::Dict{Symbol, Any} )
+function taskproducer( argDict::Dict{Symbol, Any}; originSet = Set{Vector}() )
     task = get_task( argDict[:task] )
     # set gpu id
     set!(task, :deviceID, argDict[:deviceid])
@@ -99,37 +145,13 @@ function taskproducer( argDict::Dict{Symbol, Any} )
         submit(tasks)
     elseif  contains(task[:input][:kind], "cutoutchunk") ||
             contains(task[:input][:kind], "readchunk")
-        # cut out from a big
-        N = length(argDict[:gridsize])
-        gridIndexList = Vector{Tuple}()
-        originList = Vector{Vector}()
-        for gridz in 1:argDict[:gridsize][3]
-            for gridy in 1:argDict[:gridsize][2]
-                for gridx in 1:argDict[:gridsize][1]
-                    if 3 < N
-                        # push!(gridIndexList,
-                        #         (gridx, gridy, gridz,
-                        #             ones(Int, N - 3)...))
-                        gridIndex = (gridx, gridy, gridz,
-                                        ones(Int, N - 3)...)
-                    else
-                        # push!(gridIndexList, (gridx, gridy, gridz))
-                        gridIndex = (gridx, gridy, gridz)
-                    end
-                    origin = argDict[:origin] .+ ([gridIndex...] .- 1) .* argDict[:stride]
-                    #if flag
-                        push!(originList, origin)
-                    #end
-                    if origin ==  [6091,25547,16381]
-                       flag = true
-                    end
-                end
-            end
+        if isempty( originSet )
+            originSet = get_origin_set( argDict )
         end
         # Threads.@threads for idx in gridIndexList
         #     process_task(idx)
         # end
-        map(x-> submit_chunk_task(argDict, x, task), originList)
+        map(x-> submit_chunk_task(argDict, x, task), originSet)
     else
         error("invalid input method: $(task[:input][:kind])")
     end
