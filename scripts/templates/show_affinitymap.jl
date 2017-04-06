1dcc94f2e75a7120761321d149774a0ff58366c3
jingpengwu <jingpeng.wu@gmail.com>
2017-04-06 12:13:26 -0400

    fix kaffe reading binary bug, the size should contains all the three channels of affinitymap.
    Former-commit-id: 429f206bc95d037e51abfe1a39771f06cb849217
    Former-commit-id: e65580f7e10e9522e079e963802edbb807a1102b

--- scripts/templates/show_affinitymap.jl
+++ scripts/templates/show_affinitymap.jl
@@ -15,7 +15,8 @@ ba = BigArray(d)
 # black region 2
 # affx = ba[63249:63488,  27649:30720, 16641:16768,1][:,:,:,1]
 # affx = ba[32769:33380,32769:33380,1:100,1][:,:,:,1]
-affx = ba[28161:28672,28161:28672,65:128, 1][:,:,:,1]
+# affx = ba[28161:28672,28161:28672,65:128, 1][:,:,:,1]
+aff = ba[75265:75776,19969:20480,65:128, 1:3]
 # using ImageView
 # imshow(affx)
 
@@ -23,15 +24,17 @@ using Images
 using FileIO
 
 
-for z in 1:size(affx, 3)
-    @show z
-    img = affx[:,:,z]
-    for i in eachindex(img)
-        if img[i]>Float32(1)
-            img[i] = Float32(1)
-        elseif img[i] <Float32(0)
-            img[i] = Float32(0)
-        end
+for c in 1:3
+    for z in 1:size(aff, 3)
+        @show z
+        img = aff[:,:,z,c]
+        # for i in eachindex(img)
+        #     if img[i]>Float32(1)
+        #         img[i] = Float32(1)
+        #     elseif img[i] <Float32(0)
+        #         img[i] = Float32(0)
+        #     end
+        # end
+        save(expanduser("~/pinky/aff_$(z)_$c.png"), img)
     end
-    save(expanduser("~/pinky/affx_$(z).png"), img)
 end
