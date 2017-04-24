2302a88ea6d698f120fe52f9ac68c11f1c6a7d5d
jingpengwu <jingpeng.wu@gmail.com>
2017-04-24 19:25:45 -0400

    add save png function
    Former-commit-id: 69c05bfb6e2c756fc5dd1b792de1622db2921cef
    Former-commit-id: 0ba70493033e2ba0c8dc06c0e1563b899d8fe1d8

---
+++ src/utils/polygon.jl
@@ -0,0 +1,37 @@
+
+include(joinpath(Pkg.dir(), "ImageRegistration/src/geometry.jl"))
+
+using DataStructures
+
+const DEFAUL_POLYGON = [10276 8116;
+                        10276 43716;                                                                                 
+                        65016 43716;                                                                                 
+                        65016 4416;                                                                                  
+                        10276 8116]
+"""
+    polygon_filter( originSet::Set{Vector}, polygon::Array{Int,2};
+                            blockSize::NTuple{Int,2} = (512,512) )
+only keep the origins, whose corresponding block is completely inside the polygon
+"""
+function polygon_filter(    originSet::OrderedSet{Vector};
+                            polygon::Array{Int,2} = DEFAUL_POLYGON,
+                            blockSize::Vector{Int} = [512,512] )
+    @assert size(polygon, 2) == 2
+    @assert polygon[1,:] == polygon[end, :]
+
+    ret = Set{Vector}()
+    for origin in originSet
+        p1 = origin[1:2]
+        p2 = [origin[1]                       (origin[2] + blockSize[2] -1)]
+        p3 = [(origin[1] + blockSize[1] -1)   origin[2]]
+        p4 = [(origin[1] + blockSize[1] -1)   (origin[2] + blockSize[2] -1)]
+        #@show p1, p2, p3, p4
+        #@show polygon
+        if  pt_in_poly( p1, polygon ) && pt_in_poly( p2, polygon ) &&
+            pt_in_poly( p3, polygon ) && pt_in_poly( p4, polygon )
+            push!(ret, origin)
+        end
+    end
+    
+    return ret
+end
