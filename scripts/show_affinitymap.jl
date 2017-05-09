1e6372f62c2ee04091f654287732e84f31544b4d
jingpengwu <jingpeng.wu@gmail.com>
2017-05-08 21:40:18 -0400

    return normal state when there is no tasks
    Former-commit-id: 0ed53828a1806dbad72330c97ef4c61ef281e90a

--- scripts/show_affinitymap.jl
+++ scripts/show_affinitymap.jl
@@ -10,51 +10,18 @@ using S3Dicts
 #h5write(expanduser("~/test.img.h5"), "main", img)
 #quit()
 
-#d = S3Dict("s3://neuroglancer/pinky40_v3/semanticmap/4_4_40/")
-d = S3Dict("s3://neuroglancer/pinky40_v11/affinitymap-jnet/4_4_40/")
+d = S3Dict("s3://neuroglancer/pinky40_v11/semanticmap-4/4_4_40/")
+#d = S3Dict("s3://neuroglancer/pinky40_v11/affinitymap-jnet/4_4_40/")
 
 ba = BigArray(d)
 # bb = boundingbox(ba)
 # @show bb.start
 # @show bb
 
-# black region 1
-# affx = ba [63249:63488,  26625:32768, 16897:17152, 1][:,:,:,1]
-# affx = ba[63248:63489,  26624:32769, 16896:17151, 1][:,:,:,1]
+aff = ba[14337:14337+1024-1, 7681:7681+1024-1, 65:128, 1]
 
-# black region 2
-# affx = ba[63249:63488,  27649:30720, 16641:16768,1][:,:,:,1]
-# affx = ba[32769:33380,32769:33380,1:100,1][:,:,:,1]
-# affx = ba[28161:28672,28161:28672,65:128, 1][:,:,:,1]
-# aff = ba[75265:75776,19969:20480,65:128, 1:3]
-# aff = ba[79361:79872, 20993:21504, 1:128, 1:3]
-# aff = ba[72704:73216,20480:20992, 128:192]
-# aff = ba[26625:26625+512, 26625:26625+512, 1:64, 1:3]
-# aff = ba[79872+1:79872+1024,20993:20992+1024,65:128+64, 1:3]
-#aff = ba[83457:83968, 33281:33792, 129:256, 1:3]
-#aff = ba[9727-4096:10240-4096, 31233+4096:31744+4096, 65:192, 1:3]
-#aff = ba[10241:10752, 26113:26624, 129:256, 1:3]
-# aff = ba[36353+2048:36864+2048+512+1024, 58369+2048:58880+2048+512+1024, 321-64:384, 1:4]
-
-# aff = ba[36765:36960, 36900:37100, 110:130, 1:3]
-
-
-#aff = ba[40850:41070, 37047:37240, 108:120, 1]
-#aff = ba[36853:36873, 36900:37100, 108:120, 1]
-
-#aff = ba[29000:29800, 24000:25000, 464, 1]
-
-#aff = ba[23933:24733, 24545:26593, 1:128, 1]
-
-#aff = ba[10650:11650, 13088:14088, 50:100, 1]
-
-#aff = ba[40534: 42534, 29866:31866, 290:300, 1]
-aff = ba[40701:42701, 29978:31978, 182:192, 1]
-
-# aff = ba[40961:40961+1024-1, 36865:36865+1024-1, 1:128, 1:3]
-# aff[11, :,:,:] = Float32(0)
-# using HDF5
-# h5write("/usr/people/jingpeng/test.sem.h5", "main", aff)
+#using HDF5
+#h5write("/usr/people/jingpeng/pinky/test.aff.h5", "main", aff)
 
 
 # using ImageView
@@ -68,13 +35,7 @@ for c in 1:size(aff, 4)
     for z in 1:size(aff, 3)
         @show z
         img = aff[:,:,z,c]
-        # for i in eachindex(img)
-        #     if img[i]>Float32(1)
-        #         img[i] = Float32(1)
-        #     elseif img[i] <Float32(0)
-        #         img[i] = Float32(0)
-        #     end
-        # end
-        save(expanduser("~/pinky/aff_$(z)_$c.png"), img)
+        @show size(img)
+        save(expanduser("~/pinky/section_$(z)_$c.png"), img)
     end
 end
