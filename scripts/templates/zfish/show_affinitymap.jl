46abf5e83eb9b455f04e2e8917fc72823c0b50e5
jingpengwu <jingpeng.wu@gmail.com>
2017-05-22 13:27:21 -0400

    add some pipeline configure file including pinky40 and zfish
    Former-commit-id: f65a9421c8654ea188a9942c61d0a9e367c74fd1

--- scripts/templates/zfish/show_affinitymap.jl
+++ scripts/templates/zfish/show_affinitymap.jl
@@ -1,19 +1,12 @@
 using BigArrays
 
 using BigArrays.H5sBigArrays
+ba = H5sBigArray(expanduser("~/seungmount/research/Jingpeng/14_zfish/affinitymap/"));
+affx = ba[56321-16384:56321-16384+512-1,  33281-16384:33281-16384+512-1, 33921-16384:33921-16384+128-1,3:3][:,:,:,1]
 
-ba = H5sBigArray("~/seungmount/research/Jingpeng/14_zfish/affinitymap/");
-# bb = boundingbox(ba)
-# @show bb.start
-# @show bb
-
-# black region 1
-# affx = ba [63249:63488,  26625:32768, 16897:17152, 1][:,:,:,1]
-# affx = ba[63248:63489,  26624:32769, 16896:17151, 1][:,:,:,1]
-
-# black region 2
-# affx = ba[63249:63488,  27649:30720, 16641:16768,1][:,:,:,1]
-affx = ba[63248:63489,  27648:30721, 16640:16769,1][:,:,:,1]
+#using S3Dicts
+#ba = BigArray( S3Dict("s3://neuroglancer/zfish_v2/affinitymap/5_5_45/") )
+#affx = ba[56321:56321+512-1, 33281:33281+512-1, 33921:33921+128-1, 1][:,:,:,1]
 
 # using ImageView
 # imshow(affx)
@@ -25,12 +18,5 @@ using FileIO
 for z in 1:size(affx, 3)
     @show z
     img = affx[:,:,z]
-    for i in eachindex(img)
-        if img[i]>Float32(1)
-            img[i] = Float32(1)
-        elseif img[i] <Float32(0)
-            img[i] = Float32(0)
-        end
-    end
     save(expanduser("~/zfish/affx_$(z).png"), img)
 end
