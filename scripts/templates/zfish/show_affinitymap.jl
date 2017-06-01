ee4c1ac9a05025e75b67fd06572973912cd2f158
jingpengwu <jingpeng.wu@gmail.com>
2017-06-01 17:17:53 -0400

    add some templates
    Former-commit-id: fe9a30865d04a33a29fad69bd7ce7d8e1ac2ef1a

--- scripts/templates/zfish/show_affinitymap.jl
+++ scripts/templates/zfish/show_affinitymap.jl
@@ -2,7 +2,7 @@ using BigArrays
 
 using BigArrays.H5sBigArrays
 ba = H5sBigArray(expanduser("~/seungmount/research/Jingpeng/14_zfish/affinitymap/"));
-affx = ba[56321-16384:56321-16384+512-1,  33281-16384:33281-16384+512-1, 33921-16384:33921-16384+128-1,3:3][:,:,:,1]
+#affx = ba[56321-16384:56321-16384+512-1,  33281-16384:33281-16384+512-1, 33921-16384:33921-16384+128-1,3:3][:,:,:,1]
 
 #using S3Dicts
 #ba = BigArray( S3Dict("s3://neuroglancer/zfish_v2/affinitymap/5_5_45/") )
@@ -11,12 +11,16 @@ affx = ba[56321-16384:56321-16384+512-1,  33281-16384:33281-16384+512-1, 33921-1
 # using ImageView
 # imshow(affx)
 
+#using GSDicts
+#ba = BigArray( GSDict("gs://neuroglancer/zfish") )
+
+affx = ba[62411:62411+250, 27595:27595+3082, 16637:16637+138, 1 ][:,:,:,1]
+
 using Images
 using FileIO
 
-
-for z in 1:size(affx, 3)
-    @show z
-    img = affx[:,:,z]
-    save(expanduser("~/zfish/affx_$(z).png"), img)
-end
+@sync begin
+    for z in 1:size(affx, 3)
+        @async save(expanduser("~/zfish/affx_$(z).png"), affx[:,:,z])
+    end
+end 
