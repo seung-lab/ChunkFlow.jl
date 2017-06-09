832865c7d6758282e43dde90ad02f0e72c150d9c
jingpengwu <jingpeng.wu@gmail.com>
2017-06-09 13:04:49 -0400

    move documentation from wiki to folder; add some more templates
    Former-commit-id: ae0a4a76bd1f20367984fdfd340c4701a903b517

--- scripts/show_affinitymap.jl
+++ scripts/show_affinitymap.jl
@@ -1,6 +1,11 @@
 using BigArrays
-
+using GSDicts
 using S3Dicts
+
+#ba = BigArray( GSDict("gs://neuroglancer/pinky40_v11/qaffinitymap-jnet-x/128_128_40/") )
+
+#aff = ba[321:321+1792-1, 129:129+1280-1, 1:1004, 1]
+
 
 #di = S3Dict("s3://neuroglancer/pinky40_v3/image/4_4_40/")
 #bai = BigArray(di)
@@ -10,33 +15,39 @@ using S3Dicts
 #h5write(expanduser("~/test.img.h5"), "main", img)
 #quit()
 
-d = S3Dict("s3://neuroglancer/pinky40_v11/semanticmap-4/4_4_40/")
+#d = S3Dict("s3://neuroglancer/pinky40_v11/semanticmap-4/4_4_40/")
 #d = S3Dict("s3://neuroglancer/pinky40_v11/affinitymap-jnet/4_4_40/")
 
-ba = BigArray(d)
-# bb = boundingbox(ba)
-# @show bb.start
-# @show bb
+ba = BigArray( S3Dict("s3://neuroglancer/zfish_v2/affinitymap/5_5_45/") )
 
+using BigArrays.H5sBigArrays
+#ba = H5sBigArray("/usr/people/jingpeng/seungmount/research/Jingpeng/14_zfish/affinitymap/")
+aff = ba[51393-200:51393+833, 20929+50:20929+577, 17961+23:17961+88, 1]
 # aff = ba[14337:14337+1024-1, 7681:7681+1024-1, 65:128, 1]
-aff = ba[22817:22817+1024-1, 22960:22960+1024-1, 513:513+63, 1]
+#aff = ba[19457:19457+1024-1, 19457:19457+1024-1, 16513:16513+128-1, 1:3]
 
-#using HDF5
-#h5write("/usr/people/jingpeng/pinky/test.aff.h5", "main", aff)
-
-
+using HDF5
+#h5write("/usr/people/jingpeng/pinky40/affinitymap/aff_128_128_40.h5", "main", aff)
 # using ImageView
 # imshow(affx)
 
 using Images
 using FileIO
 
+# transform int type in case the affinitymap was quantized
+if eltype(aff) == UInt8
+    aff = reinterpret(N0f8, aff)
+end 
 
 for c in 1:size(aff, 4)
-    for z in 1:size(aff, 3)
-        @show z
-        img = aff[:,:,z,c]
-        @show size(img)
-        save(expanduser("~/pinky/section_$(z)_$c.png"), img)
-    end
+    @sync begin
+        for z in 1:size(aff, 3)
+            @async begin 
+                @show z
+                img = aff[:,:,z,c]
+                @show size(img)
+                save(expanduser("~/Downloads/affinitymap/section_$(z)_$c.png"), img)
+            end
+        end
+    end 
 end
--- scripts/show_image.jl
+++ scripts/show_image.jl
@@ -1,26 +1,42 @@
 using S3Dicts
+using GSDicts
 using BigArrays
-
-ba = BigArray( S3Dict("s3://neuroglancer/pinky40_v10/image/4_4_40/") )
+using BigArrays.H5sBigArrays
+using BigArrays.AlignedBigArrays
+#ba = BigArray( S3Dict("s3://neuroglancer/pinky40_v10/image/4_4_40/") )
+#ba = AlignedBigArray( "/mnt/data01/datasets/zebrafish/4_aligned/" )
 
 
 #vol = ba[28161:28672, 25089:25600, 65:128]
 #vol = ba[15873:16384, 31745:32256, 65:128]
 #vol = ba[31053:32453, 39336:40336,   212:218]
 #vol = ba[18193:19193, 37789:39789, 213:216]
-vol = ba[14337:14337+2112-1, 7681:7681+2112-1, 1:136]
+#vol = ba[20481:20992, 28673:29184, 17793:17856]
+
+#ba = BigArray( GSDict("gs://neuroglancer/pinky40_v11/qsemanticmap-5-x/128_128_40/") )
+#vol = ba[321:321+1792-1, 129:129+1280-1, 1:1004]
+
+#ba = BigArray( GSDict("gs://neuroglancer/pinky40_v11/image/16_16_40/") )
+#vol = ba[2561:2561+1024-1, 1025+10240-1024:1025+10240-1, 1:1004]
+#vol = ba[2561+14336-1024:2561+13824-1, 1025+10240-1024:1025+10240-1, 1:1004]
+
+ba = BigArray( GSDict("gs://neuroglancer/zfish_v1/image/5_5_45/") )
+vol = ba[51393:52416, 20929:21952, 17961:18088]
 
 using HDF5
-h5write("/usr/people/jingpeng/pinky/image.h5", "main", vol)
+#h5write("/usr/people/jingpeng/pinky40/semanticmap_all.h5", "main", vol)
+#vol = h5read("/usr/people/jingpeng/pinky40/image_chunk_2.h5", "main")
 
 using Images
 using FileIO
 
 vol = reinterpret(N0f8, vol)
-for z in 1:size(vol, 3)
-    @show z
-    img = vol[:,:,z]
-    @show size(img)
-    save(expanduser("~/pinky/image_$(z).png"), img )
+@sync begin
+    for z in 1:size(vol, 3)
+        @async begin 
+            @show z 
+            save(expanduser("~/Downloads/images/image_$(z).png"), vol[:,:,z])
+        end 
+    end
 end
 
