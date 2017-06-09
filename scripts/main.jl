3e3a1a38bd806f0b9cf10e9173446c9faa389d74
jingpengwu <jingpeng.wu@gmail.com>
2017-06-09 13:04:49 -0400

    move documentation from wiki to folder; add some more templates
    Former-commit-id: 001eead0bc4de7acdd9efb47a6891a959f5486f5
    Former-commit-id: c2bdfecfefb5c19229dda05c86f6145301cd2740

--- scripts/main.jl
+++ scripts/main.jl
@@ -1,10 +1,14 @@
 # include("../src/core/argparser.jl")
 # include("../src/ChunkFlow.jl")
-using Agglomeration, Process
-@everywhere using Agglomeration, Process
-@everywhere using ChunkFlow
-@everywhere using ChunkFlow.Execute
-@everywhere using Logging
+
+# sleep a little and let the process fully launches
+# https://github.com/JuliaLang/julia/issues/12381
+@everywhere sleep(0.1)
+
+import Agglomeration, Process; @everywhere using Agglomeration, Process
+import ChunkFlow; @everywhere using ChunkFlow
+import ChunkFlow.Execute; @everywhere using ChunkFlow.Execute
+import Logging; @everywhere using Logging
 
 @Logging.configure(level=INFO)
 Logging.configure(filename="$(tempname()).log")
