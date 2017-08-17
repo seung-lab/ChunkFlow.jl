a473306c98d6e4fd8f9997f179b8e28c4bf42da4
jingpengwu <jingpeng.wu@gmail.com>
2017-08-17 17:43:19 -0400

    lambda function works

--- python/create_lambda_function.py
+++ python/create_lambda_function.py
@@ -1,21 +1,41 @@
+#!/usr/bin/env python
+
 import boto3
+import zlib
+import zipfile
+from StringIO import StringIO
+
+
+def get_lambda_function(script_name='lambda_function.py'):
+    with open(script_name, 'r') as myfile:
+        data = myfile.read()
+    # compress with zip
+    data = zlib.compress(data)
+    return data
+
+
+def get_lambda_function_v2(script_name='lambda_function.py'):
+    buf = StringIO()
+    with zipfile.ZipFile(buf, 'w') as z:
+        z.write(script_name)
+    buf.seek(0)
+    return buf.read()
 
 client = boto3.client('lambda')
-
 client.create_function(
     FunctionName='chunkflow-inference',
     Runtime='python2.7',
-    Role='chunkflow-lambda',
+    Role='arn:aws:iam::098703261575:role/chunkflow-lambda',
     Handler='lambda_function.lambda_handler',
     Code={
-        'ZipFile': b''
+        'ZipFile': get_lambda_function_v2()
     },
     Description='lambda function to launch auto-scale spot fleet.',
     Timeout=3,
-    MemorySize=64,
+    MemorySize=128,
     Publish=True,
     VpcConfig={
-        'SubnetIds':[
+        'SubnetIds': [
             'subnet-fb3626a0'
         ],
         'SecurityGroupIds': [
