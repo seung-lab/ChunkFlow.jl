ec1c3d4acbe7c35b16efb831b58e2c77dab677b8
jingpengwu <jingpeng.wu@gmail.com>
2017-08-18 14:12:32 -0400

    add autoscaling script

--- python/create_lambda_function.py
+++ python/create_lambda_function.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python
 
 import boto3
-import zlib
 import zipfile
 from StringIO import StringIO
 
 
 def get_lambda_function(script_name='lambda_function.py'):
-    with open(script_name, 'r') as myfile:
-        data = myfile.read()
-    # compress with zip
-    data = zlib.compress(data)
-    return data
-
-
-def get_lambda_function_v2(script_name='lambda_function.py'):
     buf = StringIO()
     with zipfile.ZipFile(buf, 'w') as z:
         z.write(script_name)
@@ -22,28 +13,41 @@ def get_lambda_function_v2(script_name='lambda_function.py'):
     return buf.read()
 
 client = boto3.client('lambda')
-client.create_function(
+
+# delete the function
+# client.delete_function(FunctionName='chunkflow-inference')
+#
+# response = client.create_function(
+#     FunctionName='chunkflow-inference',
+#     Runtime='python2.7',
+#     Role='arn:aws:iam::098703261575:role/chunkflow-lambda',
+#     Handler='lambda_function.lambda_handler',
+#     Code={
+#         'ZipFile': get_lambda_function()
+#     },
+#     Description='lambda function to launch auto-scale spot fleet.',
+#     Timeout=10,
+#     MemorySize=128,
+#     Publish=True,
+#     VpcConfig={
+#         'SubnetIds': [
+#             'subnet-fb3626a0'
+#         ],
+#         'SecurityGroupIds': [
+#             'sg-84743efb'
+#         ]
+#     },
+#     Tags={
+#         'User': 'jingpeng',
+#         'Tool': 'chunkflow'
+#     }
+# )
+
+response = client.update_function_code(
     FunctionName='chunkflow-inference',
-    Runtime='python2.7',
-    Role='arn:aws:iam::098703261575:role/chunkflow-lambda',
-    Handler='lambda_function.lambda_handler',
-    Code={
-        'ZipFile': get_lambda_function_v2()
-    },
-    Description='lambda function to launch auto-scale spot fleet.',
-    Timeout=3,
-    MemorySize=128,
+    ZipFile=get_lambda_function(),
     Publish=True,
-    VpcConfig={
-        'SubnetIds': [
-            'subnet-fb3626a0'
-        ],
-        'SecurityGroupIds': [
-            'sg-84743efb'
-        ]
-    },
-    Tags={
-        'User': 'jingpeng',
-        'Tool': 'chunkflow'
-    }
+    DryRun=False
 )
+
+print('response = %s' % response)
