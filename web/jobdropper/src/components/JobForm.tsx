ffdf186a57c98c5ffd0b703f629a6a5a032c4ca2
jingpengwu <jingpeng.wu@gmail.com>
2017-09-05 00:22:46 -0400

    make material-ui work

--- web/jobdropper/src/components/JobForm.tsx
+++ web/jobdropper/src/components/JobForm.tsx
@@ -1,10 +1,23 @@
 import * as React from "react";
+// import * as MUI from 'material-ui';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {MuiThemeProvider, lightBaseTheme} from 'material-ui/styles';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 
+const lightMuiTheme = getMuiTheme(lightBaseTheme);
+
+const style = {
+  margin: 12,
+};
 
 export class JobForm extends React.Component <any, any> {
     constructor(props: any) {
         super(props);
         this.state = {
+            queue: 'chunkflow-inference',
             taskTemplate: 'task template',
             start: [1,2,3],
             gridSize: [1,2,3]
@@ -13,38 +26,41 @@ export class JobForm extends React.Component <any, any> {
         this.handleStartChange = this.handleStartChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+    handleQueueChange(event: any, index: number, value: string) {
+        alert('queue name: ' + value);
+        this.setState({queue: {value}});
+    }
     handleTaskTemplateChange(event: any) {
         this.setState({taskTemplate: event.target.taskTemplate});
     }
     handleStartChange(event: any) {
-        this.setState({start: event.target.start})
+        this.setState({start: event.target.start});
     }
     handleSubmit(event: any) {
         alert('A task was submitted: ' + this.state.taskTemplate);
         alert('the start coordinate: ' + this.state.start);
+        alert('the queue: ' + this.state.queue);
         event.preventDefault();
     }
 
     render(){
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    <h1>ChunkFlow Task Definition and Submition</h1>
-                    <p>
-                    AWS SQS queue name:
-                    <select>
-                        <option value="chunkflow-inference">chunkflow-inference</option>
-                    </select>
-                    </p>
-                    < input type="text" value={this.state.start} onChange={this.handleStartChange} />
-                    <p>
-                    <textarea name='textarea' value={this.state.taskTemplate} onChange={this.handleTaskTemplateChange}/>
-                    </p>
-                </label>
-                <p>
-                <input type="submit" value="Submit Tasks" onSubmit={this.handleSubmit}/>
-                </p>
-            </form>
+            <MuiThemeProvider muiTheme={lightMuiTheme}>
+            <div>
+                AWS Queue Name: <br/>
+                <DropDownMenu value={this.state.queue} onChange={this.handleQueueChange}>
+                    <MenuItem value={"chunkflow-inference"} primaryText="chunkflow-inference"/>
+                    <MenuItem value={"chunkflow-ingest"} primaryText="chunkflow-ingest"/>
+                </DropDownMenu><br/>
+                <TextField hintText="task template"
+                    floatingLabelText={this.state.taskTemplate}
+                    multiLine={true}
+                    onChange={this.handleTaskTemplateChange}
+                /><br/>
+                <RaisedButton onClick = {this.handleSubmit}
+                    label="Submit Tasks" primary={true} style={style}/>
+            </div>
+            </MuiThemeProvider>
         );
     }
 }
