66eba43f6f57d09ee75d5d042c8a8c8bacc1abdd
jingpengwu <jingpeng.wu@gmail.com>
2017-11-01 00:08:44 -0400

    tried to import Batch from custom built js sdk, but failed; remove the dependency of Batch for now, will submit jobs using command line

--- web/jobdropper/src/components/Batch.ts
+++ web/jobdropper/src/components/Batch.ts
@@ -1,17 +1,21 @@
-import AWS = require('aws-sdk');
-AWS.config.update({
-    region: process.env.AWS_REGION, 
-    credentials: new AWS.Credentials(
-        process.env.AWS_ACCESS_KEY_ID,
-        process.env.AWS_SECRET_ACCESS_KEY )
-});
+// import AWS = require('aws-sdk');
+import * as AWS from './aws-sdk-2.141.0';
+
+//AWS.config.update({
+//    region: process.env.AWS_REGION,
+//    credentials: new AWS.Credentials(
+//        process.env.AWS_ACCESS_KEY_ID,
+//        process.env.AWS_SECRET_ACCESS_KEY )
+//});
 
 let batch = new AWS.Batch({
     apiVersion: '2016-08-10',
-    region: process.env.AWS_REGION
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-export class Batch {
+export class AWSBatch {
     private _params = {
         jobDefinition: "CPUInferenceJobDefiniti-4c21ad357cc3025:1",
         jobName: "test",
@@ -35,14 +39,12 @@ export class Batch {
     constructor(jobQueue: string = "LowPriorityBatchCloudformationJobqueue",
                 sqsQueue: string = "chunkflow-inference"){
         this._params.containerOverrides.command = ["julia main.jl -q " + sqsQueue];
-        this._params.jobQueue = jobQueue; 
+        this._params.jobQueue = jobQueue;
     }
     submit_job(){
-        batch.submitJob(this._params, function(err, data){
+        batch.submitJob(this._params, function(err:any, data:any){
             if (err) console.log(err, err.stack);
             else    console.log(data);
         });
     }
 }
-
-
